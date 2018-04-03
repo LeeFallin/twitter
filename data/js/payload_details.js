67f7252440ab05c3db89cdf5ca3afeca45c19aa0
LeeFallin <lee@newgecko.net>
2018-04-03 01:21:19 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 5691,
-	"created_at": "Mon Apr 02 00:21:17 +0000 2018",
+	"tweets": 5692,
+	"created_at": "Tue Apr 03 00:21:17 +0000 2018",
 	"lang": "en-gb"
 }
--- data/js/tweets/2018_04.js
+++ data/js/tweets/2018_04.js
@@ -1,6 +1,214 @@
 Grailbird.data.tweets_2018_04 = 
 [
 	{
+		"created_at": "Mon Apr 02 18:02:59 +0000 2018",
+		"id": 980868209304920000,
+		"id_str": "980868209304920069",
+		"text": "New (quick) blog post: How to #mindmap a #keynote or #lecture #notetaking https://t.co/vQZtj6hRUt https://t.co/urVpzROVkO",
+		"truncated": false,
+		"entities": {
+			"hashtags": [
+				{
+					"text": "mindmap",
+					"indices": [
+						30,
+						38
+					]
+				},
+				{
+					"text": "keynote",
+					"indices": [
+						41,
+						49
+					]
+				},
+				{
+					"text": "lecture",
+					"indices": [
+						53,
+						61
+					]
+				},
+				{
+					"text": "notetaking",
+					"indices": [
+						62,
+						73
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/vQZtj6hRUt",
+					"expanded_url": "https://leefallin.co.uk/2018/04/how-to-mindmap-a-keynote-or-lecture/",
+					"display_url": "leefallin.co.uk/2018/04/how-to…",
+					"indices": [
+						74,
+						97
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 980868206775562200,
+					"id_str": "980868206775562240",
+					"indices": [
+						98,
+						121
+					],
+					"media_url": "http://pbs.twimg.com/media/DZy-b-xU8AAma2N.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/DZy-b-xU8AAma2N.jpg",
+					"url": "https://t.co/urVpzROVkO",
+					"display_url": "pic.twitter.com/urVpzROVkO",
+					"expanded_url": "https://twitter.com/LeeFallin/status/980868209304920069/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 658,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1024,
+							"h": 991,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1024,
+							"h": 991,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 980868206775562200,
+					"id_str": "980868206775562240",
+					"indices": [
+						98,
+						121
+					],
+					"media_url": "http://pbs.twimg.com/media/DZy-b-xU8AAma2N.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/DZy-b-xU8AAma2N.jpg",
+					"url": "https://t.co/urVpzROVkO",
+					"display_url": "pic.twitter.com/urVpzROVkO",
+					"expanded_url": "https://twitter.com/LeeFallin/status/980868209304920069/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 658,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1024,
+							"h": 991,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1024,
+							"h": 991,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://publicize.wp.com/\" rel=\"nofollow\">WordPress.com</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 55448837,
+			"id_str": "55448837",
+			"name": "Lee Fallin",
+			"screen_name": "LeeFallin",
+			"location": "United Kingdom",
+			"description": "#LearningDeveloper, #EdD student & #MIEExpert @UniofHull. I tweet #HigherEd, #EdTech, #Lego, #LDchat, #politics & #Hull. All tweets in a personal capacity.",
+			"url": "https://t.co/UWrodKaQLd",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/UWrodKaQLd",
+							"expanded_url": "http://www.leefallin.co.uk",
+							"display_url": "leefallin.co.uk",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 776,
+			"friends_count": 852,
+			"listed_count": 57,
+			"created_at": "Fri Jul 10 02:53:40 +0000 2009",
+			"favourites_count": 1492,
+			"utc_offset": 3600,
+			"time_zone": "London",
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 5688,
+			"lang": "en-gb",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/753894560108011520/7h68mawt_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/753894560108011520/7h68mawt_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/55448837/1486734016",
+			"profile_link_color": "1B95E0",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 2,
+		"favorite_count": 3,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en"
+	},
+	{
 		"created_at": "Sun Apr 01 19:02:22 +0000 2018",
 		"id": 980520764830048300,
 		"id_str": "980520764830048257",

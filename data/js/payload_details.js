36d755716e88d5255184ab8d7d82900d74ee8279
LeeFallin <lee@newgecko.net>
2019-01-04 00:21:16 +0000

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 7147,
-	"created_at": "Thu Jan 03 00:21:17 +0000 2019",
+	"tweets": 7148,
+	"created_at": "Fri Jan 04 00:21:15 +0000 2019",
 	"lang": "en-gb"
 }
--- data/js/tweets/2019_01.js
+++ data/js/tweets/2019_01.js
@@ -295,6 +295,348 @@ Grailbird.data.tweets_2019_01 =
 				}
 			},
 			"protected": false,
+			"followers_count": 3067,
+			"friends_count": 2118,
+			"listed_count": 579,
+			"created_at": "Mon Jul 13 13:06:27 +0000 2009",
+			"favourites_count": 17089,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 50957,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "352726",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme5/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme5/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/707234049144840195/oOSySzdy_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/707234049144840195/oOSySzdy_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/56366858/1479122559",
+			"profile_link_color": "D02B55",
+			"profile_sidebar_border_color": "829D5E",
+			"profile_sidebar_fill_color": "99CC33",
+			"profile_text_color": "3E4415",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 2,
+		"favorite_count": 8,
+		"favorited": true,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "#LTHEchat via NodeXL https://t.co/XbnsPiBClE\n@lthechat\n@phillipdawson\n@suebecks\n@leefallin\n@nomadwarmachine\n@scottturneruon\n@racephil\n@advancehe_chat\n@santanuvasant\n@kiusum\n\nTop hashtags:\n#lthechat\n#advancehe_chat\n#qaafocuson\n#socmedhe18\n#altc\n#coachinghe\n#socmedhe https://t.co/ZyP0rznQmD"
+	},
+	{
+		"created_at": "Wed Jan 02 12:00:02 +0000 2019",
+		"id": 1080433531690602500,
+		"id_str": "1080433531690602496",
+		"full_text": "#LTHEchat via NodeXL https://t.co/XbnsPiBClE\n@lthechat\n@phillipdawson\n@suebecks\n@leefallin\n@nomadwarmachine\n@scottturneruon\n@racephil\n@advancehe_chat\n@santanuvasant\n@kiusum\n\nTop hashtags:\n#lthechat\n#advancehe_chat\n#qaafocuson\n#socmedhe18\n#altc\n#coachinghe\n#socmedhe https://t.co/ZyP0rznQmD",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			265
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "LTHEchat",
+					"indices": [
+						0,
+						9
+					]
+				},
+				{
+					"text": "lthechat",
+					"indices": [
+						188,
+						197
+					]
+				},
+				{
+					"text": "advancehe_chat",
+					"indices": [
+						198,
+						213
+					]
+				},
+				{
+					"text": "qaafocuson",
+					"indices": [
+						214,
+						225
+					]
+				},
+				{
+					"text": "socmedhe18",
+					"indices": [
+						226,
+						237
+					]
+				},
+				{
+					"text": "altc",
+					"indices": [
+						238,
+						243
+					]
+				},
+				{
+					"text": "coachinghe",
+					"indices": [
+						244,
+						255
+					]
+				},
+				{
+					"text": "socmedhe",
+					"indices": [
+						256,
+						265
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "LTHEchat",
+					"name": "LTHE Tweetchat",
+					"id": 2659221798,
+					"id_str": "2659221798",
+					"indices": [
+						45,
+						54
+					]
+				},
+				{
+					"screen_name": "phillipdawson",
+					"name": "Phillip Dawson",
+					"id": 17531694,
+					"id_str": "17531694",
+					"indices": [
+						55,
+						69
+					]
+				},
+				{
+					"screen_name": "suebecks",
+					"name": "Sue Beckingham",
+					"id": 34904126,
+					"id_str": "34904126",
+					"indices": [
+						70,
+						79
+					]
+				},
+				{
+					"screen_name": "LeeFallin",
+					"name": "Lee Fallin",
+					"id": 55448837,
+					"id_str": "55448837",
+					"indices": [
+						80,
+						90
+					]
+				},
+				{
+					"screen_name": "NomadWarMachine",
+					"name": "Sarah Honeychurch",
+					"id": 558091832,
+					"id_str": "558091832",
+					"indices": [
+						91,
+						107
+					]
+				},
+				{
+					"screen_name": "scottturneruon",
+					"name": "Dr Scott Turner",
+					"id": 56366858,
+					"id_str": "56366858",
+					"indices": [
+						108,
+						123
+					]
+				},
+				{
+					"screen_name": "RacePhil",
+					"name": "Phil Race",
+					"id": 1350065268,
+					"id_str": "1350065268",
+					"indices": [
+						124,
+						133
+					]
+				},
+				{
+					"screen_name": "AdvanceHE_chat",
+					"name": "#AdvanceHE_chat",
+					"id": 3726587775,
+					"id_str": "3726587775",
+					"indices": [
+						134,
+						149
+					]
+				},
+				{
+					"screen_name": "santanuvasant",
+					"name": "Santanu Vasant",
+					"id": 23588538,
+					"id_str": "23588538",
+					"indices": [
+						150,
+						164
+					]
+				},
+				{
+					"screen_name": "KiuSum",
+					"name": "Kiu Sum",
+					"id": 246951711,
+					"id_str": "246951711",
+					"indices": [
+						165,
+						172
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/XbnsPiBClE",
+					"expanded_url": "https://buff.ly/2BRMG9L",
+					"display_url": "buff.ly/2BRMG9L",
+					"indices": [
+						21,
+						44
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1080433529547358200,
+					"id_str": "1080433529547358208",
+					"indices": [
+						266,
+						289
+					],
+					"media_url": "http://pbs.twimg.com/media/Dv54khwXgAAD2VB.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Dv54khwXgAAD2VB.jpg",
+					"url": "https://t.co/ZyP0rznQmD",
+					"display_url": "pic.twitter.com/ZyP0rznQmD",
+					"expanded_url": "https://twitter.com/scottturneruon/status/1080433531690602496/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 950,
+							"h": 688,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 492,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 950,
+							"h": 688,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1080433529547358200,
+					"id_str": "1080433529547358208",
+					"indices": [
+						266,
+						289
+					],
+					"media_url": "http://pbs.twimg.com/media/Dv54khwXgAAD2VB.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Dv54khwXgAAD2VB.jpg",
+					"url": "https://t.co/ZyP0rznQmD",
+					"display_url": "pic.twitter.com/ZyP0rznQmD",
+					"expanded_url": "https://twitter.com/scottturneruon/status/1080433531690602496/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 950,
+							"h": 688,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 492,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 950,
+							"h": 688,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://buffer.com\" rel=\"nofollow\">Buffer</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 56366858,
+			"id_str": "56366858",
+			"name": "Dr Scott Turner",
+			"screen_name": "scottturneruon",
+			"location": "Northamptonshire",
+			"description": "Computing Lecturer, Robots, computing education. Pi Cert Educator, code club, Member @TheIET @bcs , Views are authors only, RTs do not imply agreement",
+			"url": "https://t.co/ZdQo9bNfEs",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/ZdQo9bNfEs",
+							"expanded_url": "http://scottjturner.weebly.com/",
+							"display_url": "scottjturner.weebly.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
 			"followers_count": 3061,
 			"friends_count": 2106,
 			"listed_count": 579,

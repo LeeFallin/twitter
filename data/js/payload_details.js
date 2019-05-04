db6a87bdacb91c6c73ed860e2d9879c9566e726d
LeeFallin <lee@newgecko.net>
2019-05-04 01:21:17 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 8090,
-	"created_at": "Fri May 03 00:21:16 +0000 2019",
+	"tweets": 8091,
+	"created_at": "Sat May 04 00:21:16 +0000 2019",
 	"lang": "en-gb"
 }
--- data/js/tweets/2019_05.js
+++ data/js/tweets/2019_05.js
@@ -1,6 +1,152 @@
 Grailbird.data.tweets_2019_05 = 
 [
 	{
+		"created_at": "Fri May 03 08:57:06 +0000 2019",
+		"id": 1124236428668547100,
+		"id_str": "1124236428668547072",
+		"full_text": "@Tharindu__ @suewatling @aldinhe_LH @StudyUCEM Look forward to seeing what you do with it @Tharindu__",
+		"truncated": false,
+		"display_text_range": [
+			47,
+			101
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "Tharindu__",
+					"name": "Tharindu",
+					"id": 1580531887,
+					"id_str": "1580531887",
+					"indices": [
+						0,
+						11
+					]
+				},
+				{
+					"screen_name": "suewatling",
+					"name": "Sue Watling🌱",
+					"id": 14897464,
+					"id_str": "14897464",
+					"indices": [
+						12,
+						23
+					]
+				},
+				{
+					"screen_name": "aldinhe_LH",
+					"name": "ALDinHE",
+					"id": 119064069,
+					"id_str": "119064069",
+					"indices": [
+						24,
+						35
+					]
+				},
+				{
+					"screen_name": "StudyUCEM",
+					"name": "UCEM",
+					"id": 475049431,
+					"id_str": "475049431",
+					"indices": [
+						36,
+						46
+					]
+				},
+				{
+					"screen_name": "Tharindu__",
+					"name": "Tharindu",
+					"id": 1580531887,
+					"id_str": "1580531887",
+					"indices": [
+						90,
+						101
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": 1124233140715839500,
+		"in_reply_to_status_id_str": "1124233140715839493",
+		"in_reply_to_user_id": 1580531887,
+		"in_reply_to_user_id_str": "1580531887",
+		"in_reply_to_screen_name": "Tharindu__",
+		"user": {
+			"id": 55448837,
+			"id_str": "55448837",
+			"name": "Lee Fallin",
+			"screen_name": "LeeFallin",
+			"location": "United Kingdom",
+			"description": "SFHEA #LearningDeveloper, #EdD student, #MCE & #MIEExpert @UniofHull. I tweet #HigherEd, #EdTech, #Lego, #LDchat & #Hull. All tweets in a personal capacity",
+			"url": "https://t.co/UWrodKaQLd",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/UWrodKaQLd",
+							"expanded_url": "http://www.leefallin.co.uk",
+							"display_url": "leefallin.co.uk",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 1122,
+			"friends_count": 1048,
+			"listed_count": 64,
+			"created_at": "Fri Jul 10 02:53:40 +0000 2009",
+			"favourites_count": 4058,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 8038,
+			"lang": "en-gb",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/753894560108011520/7h68mawt_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/753894560108011520/7h68mawt_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/55448837/1555627842",
+			"profile_link_color": "1B95E0",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "en",
+		"text": "@Tharindu__ @suewatling @aldinhe_LH @StudyUCEM Look forward to seeing what you do with it @Tharindu__"
+	},
+	{
 		"created_at": "Thu May 02 13:48:12 +0000 2019",
 		"id": 1123945719948283900,
 		"id_str": "1123947298994761729",

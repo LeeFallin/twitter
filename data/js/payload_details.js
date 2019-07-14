2bbe62e3fe935769ea7ab821cdff1ecb1d6a8026
LeeFallin <lee@newgecko.net>
2019-07-14 01:21:17 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 8803,
-	"created_at": "Fri Jul 12 00:21:16 +0000 2019",
+	"tweets": 8804,
+	"created_at": "Sun Jul 14 00:21:16 +0000 2019",
 	"lang": "en-gb"
 }
--- data/js/tweets/2019_07.js
+++ data/js/tweets/2019_07.js
@@ -1,6 +1,120 @@
 Grailbird.data.tweets_2019_07 = 
 [
 	{
+		"created_at": "Sat Jul 13 11:37:07 +0000 2019",
+		"id": 1149409393689026600,
+		"id_str": "1150006236953436160",
+		"full_text": ".@MicrosoftTeams reaches 13 million daily active users! Check out 4 new ways to help your teams communicate and collaborate better. #MSInspire",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			142
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "MSInspire",
+					"indices": [
+						132,
+						142
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "MicrosoftTeams",
+					"name": "Microsoft Teams",
+					"id": 817461289081409500,
+					"id_str": "817461289081409536",
+					"indices": [
+						1,
+						16
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"https://ads-api.twitter.com\" rel=\"nofollow\">Twitter Ads Composer</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 740240040819753000,
+			"id_str": "740240040819752960",
+			"name": "Microsoft 365 news",
+			"screen_name": "MSFT365news",
+			"location": "Redmond, WA",
+			"description": "Get the latest on Office 365, Windows 10, and Enterprise Mobility + Security. Always improving to empower everyone to be creative and work together, securely.",
+			"url": "https://t.co/gousJXc0pc",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/gousJXc0pc",
+							"expanded_url": "http://microsoft.com/microsoft-365",
+							"display_url": "microsoft.com/microsoft-365",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 131019,
+			"friends_count": 1113,
+			"listed_count": 624,
+			"created_at": "Tue Jun 07 17:52:33 +0000 2016",
+			"favourites_count": 128,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 3762,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1065039796702068736/iblLs372_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1065039796702068736/iblLs372_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/740240040819752960/1557373980",
+			"profile_link_color": "0078D4",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 184,
+		"favorite_count": 580,
+		"favorited": true,
+		"retweeted": true,
+		"lang": "en",
+		"text": ".@MicrosoftTeams reaches 13 million daily active users! Check out 4 new ways to help your teams communicate and collaborate better. #MSInspire"
+	},
+	{
 		"created_at": "Thu Jul 11 16:23:42 +0000 2019",
 		"id": 1149353582526967800,
 		"id_str": "1149353582526967809",
@@ -49,7 +163,7 @@ Grailbird.data.tweets_2019_07 =
 			"name": "Lee Fallin",
 			"screen_name": "LeeFallin",
 			"location": "United Kingdom",
-			"description": "SFHEA #LearningDeveloper, #EdD student, #MCE & #MIEExpert @UniofHull. I tweet #HigherEd, #EdTech, #Lego, #LDchat & #Hull. All tweets in a personal capacity 🏳️‍🌈",
+			"description": "SFHEA #LearningDeveloper, #EdD student, #MCE & #MIEExpert @UniofHull. I tweet #HigherEd, #EdTech, #Lego, #LDchat & #Hull. All tweets in a personal capacity 🏳️🌈",
 			"url": "https://t.co/UWrodKaQLd",
 			"entities": {
 				"url": {

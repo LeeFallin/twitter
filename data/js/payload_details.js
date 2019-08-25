5707f0c4b304580abf9aad7a7f81ec7e3e9139a1
LeeFallin <lee@newgecko.net>
2019-08-25 01:21:16 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 8871,
-	"created_at": "Fri Aug 23 00:21:16 +0000 2019",
+	"tweets": 8872,
+	"created_at": "Sun Aug 25 00:21:16 +0000 2019",
 	"lang": "en-gb"
 }
--- data/js/tweets/2019_08.js
+++ data/js/tweets/2019_08.js
@@ -1,6 +1,196 @@
 Grailbird.data.tweets_2019_08 = 
 [
 	{
+		"created_at": "Sun Aug 25 00:08:00 +0000 2019",
+		"id": 1127829137090072600,
+		"id_str": "1165415493471264768",
+		"full_text": "Mental health during your #PhD! This should be in every lab, university, library, etc, and also be given by default to every supervisor. Some of our work is resource management and we must remember that *people* are our most valuable resource. @AcademicChatter @firstphdchat https://t.co/Q9TLU3umrJ",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			274
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "PhD",
+					"indices": [
+						26,
+						30
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "AcademicChatter",
+					"name": "Academic Chatter",
+					"id": 885953601067208700,
+					"id_str": "885953601067208705",
+					"indices": [
+						244,
+						260
+					]
+				},
+				{
+					"screen_name": "firstphdchat",
+					"name": "#phdchat",
+					"id": 804955748090478600,
+					"id_str": "804955748090478592",
+					"indices": [
+						261,
+						274
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 1127829121038524400,
+					"id_str": "1127829121038524421",
+					"indices": [
+						275,
+						298
+					],
+					"media_url": "http://pbs.twimg.com/media/D6banC7XoAUIWr2.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D6banC7XoAUIWr2.jpg",
+					"url": "https://t.co/Q9TLU3umrJ",
+					"display_url": "pic.twitter.com/Q9TLU3umrJ",
+					"expanded_url": "https://twitter.com/magicphddragon/status/1127829137090072576/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 848,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 481,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1448,
+							"h": 2048,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1127829121038524400,
+					"id_str": "1127829121038524421",
+					"indices": [
+						275,
+						298
+					],
+					"media_url": "http://pbs.twimg.com/media/D6banC7XoAUIWr2.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D6banC7XoAUIWr2.jpg",
+					"url": "https://t.co/Q9TLU3umrJ",
+					"display_url": "pic.twitter.com/Q9TLU3umrJ",
+					"expanded_url": "https://twitter.com/magicphddragon/status/1127829137090072576/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 848,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 481,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1448,
+							"h": 2048,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 741305539154579500,
+			"id_str": "741305539154579457",
+			"name": "Magical PhD Dragon #SaveTheOA",
+			"screen_name": "magicphddragon",
+			"location": "",
+			"description": "MD, physical rehabilitation medicine specialist, beast of magic and wonder. Science (and satire) communicator. INTJ.",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 833,
+			"friends_count": 1883,
+			"listed_count": 11,
+			"created_at": "Fri Jun 10 16:26:28 +0000 2016",
+			"favourites_count": 3390,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 1468,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/741306817427439616/1rTxidL2_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/741306817427439616/1rTxidL2_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/741305539154579457/1534329085",
+			"profile_link_color": "FF691F",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 253,
+		"favorite_count": 422,
+		"favorited": true,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Mental health during your #PhD! This should be in every lab, university, library, etc, and also be given by default to every supervisor. Some of our work is resource management and we must remember that *people* are our most valuable resource. @AcademicChatter @firstphdchat https://t.co/Q9TLU3umrJ"
+	},
+	{
 		"created_at": "Thu Aug 22 14:34:50 +0000 2019",
 		"id": 1164537929731051500,
 		"id_str": "1164546476929880064",

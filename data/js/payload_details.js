e1eade573142e5c5eec69266e9038e5240e67333
LeeFallin <lee@newgecko.net>
2019-09-06 01:21:16 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 8919,
-	"created_at": "Thu Sep 05 00:21:15 +0000 2019",
+	"tweets": 8920,
+	"created_at": "Fri Sep 06 00:21:15 +0000 2019",
 	"lang": "en-gb"
 }
--- data/js/tweets/2019_09.js
+++ data/js/tweets/2019_09.js
@@ -1,6 +1,142 @@
 Grailbird.data.tweets_2019_09 = 
 [
 	{
+		"created_at": "Thu Sep 05 07:45:27 +0000 2019",
+		"id": 1169495102819057700,
+		"id_str": "1169516881490980864",
+		"full_text": "@LeeFallin @suewatling @DrRachLTB @jsecker Yeah. We've had hypertext for decades and it IS time academia caught up (link permanence issues and versioning notwithstanding). And we really need to relax a bit more about referencing styles. It's not about where the comma is; it's about where the text is, after all.",
+		"truncated": false,
+		"display_text_range": [
+			43,
+			312
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "LeeFallin",
+					"name": "Lee Fallin",
+					"id": 55448837,
+					"id_str": "55448837",
+					"indices": [
+						0,
+						10
+					]
+				},
+				{
+					"screen_name": "suewatling",
+					"name": "Sue Watling🌱",
+					"id": 14897464,
+					"id_str": "14897464",
+					"indices": [
+						11,
+						22
+					]
+				},
+				{
+					"screen_name": "DrRachLTB",
+					"name": "Dr Rachel Maxwell",
+					"id": 936890580,
+					"id_str": "936890580",
+					"indices": [
+						23,
+						33
+					]
+				},
+				{
+					"screen_name": "jsecker",
+					"name": "Dr Jane Secker",
+					"id": 20975165,
+					"id_str": "20975165",
+					"indices": [
+						34,
+						42
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": 1169492347538804700,
+		"in_reply_to_status_id_str": "1169492347538804736",
+		"in_reply_to_user_id": 55448837,
+		"in_reply_to_user_id_str": "55448837",
+		"in_reply_to_screen_name": "LeeFallin",
+		"user": {
+			"id": 321347565,
+			"id_str": "321347565",
+			"name": "Steph & Nonsense",
+			"screen_name": "AVWoman",
+			"location": "Eoforwic",
+			"description": "ITrix; Librarina; Diarist; Dreamtweeter. Shelved at 005-6, 027, 042, 090, 305.3, 305.42, 335, 391, 412, 500, 720, 780, 790, 912, 942. Answers to her or them.",
+			"url": "https://t.co/glpvaa2c9l",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/glpvaa2c9l",
+							"expanded_url": "http://avwoman.co.uk/esc",
+							"display_url": "avwoman.co.uk/esc",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 1332,
+			"friends_count": 2442,
+			"listed_count": 116,
+			"created_at": "Tue Jun 21 12:33:10 +0000 2011",
+			"favourites_count": 46701,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 64698,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1137330644869165056/yYpVmoeL_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1137330644869165056/yYpVmoeL_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/321347565/1431817294",
+			"profile_link_color": "977CA6",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "2B0040",
+			"profile_text_color": "CC0000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1,
+		"favorite_count": 2,
+		"favorited": true,
+		"retweeted": true,
+		"lang": "en",
+		"text": "@LeeFallin @suewatling @DrRachLTB @jsecker Yeah. We've had hypertext for decades and it IS time academia caught up (link permanence issues and versioning notwithstanding). And we really need to relax a bit more about referencing styles. It's not about where the comma is; it's about where the text is, after all."
+	},
+	{
 		"created_at": "Wed Sep 04 17:35:24 +0000 2019",
 		"id": 1169302959462764500,
 		"id_str": "1169302959462764544",
@@ -39,7 +175,7 @@ Grailbird.data.tweets_2019_09 =
 			"name": "Lee Fallin",
 			"screen_name": "LeeFallin",
 			"location": "United Kingdom",
-			"description": "SFHEA #LearningDeveloper, #EdD student, #MCE & #MIEExpert @UniofHull. I tweet #HigherEd, #EdTech, #Lego, #LDchat & #Hull. All tweets in a personal capacity 🏳️‍🌈",
+			"description": "SFHEA #LearningDeveloper, #EdD student, #MCE & #MIEExpert @UniofHull. I tweet #HigherEd, #EdTech, #Lego, #LDchat & #Hull. All tweets in a personal capacity 🏳️🌈",
 			"url": "https://t.co/UWrodKaQLd",
 			"entities": {
 				"url": {

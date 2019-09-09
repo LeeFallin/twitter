1d901dd12245a8b6bd488260536bd8865ba0a11b
LeeFallin <lee@newgecko.net>
2019-09-09 01:21:16 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 8920,
-	"created_at": "Fri Sep 06 00:21:15 +0000 2019",
+	"tweets": 8921,
+	"created_at": "Mon Sep 09 00:21:15 +0000 2019",
 	"lang": "en-gb"
 }
--- data/js/tweets/2019_09.js
+++ data/js/tweets/2019_09.js
@@ -1,6 +1,219 @@
 Grailbird.data.tweets_2019_09 = 
 [
 	{
+		"created_at": "Sun Sep 08 06:44:47 +0000 2019",
+		"id": 1170039261212680200,
+		"id_str": "1170588777510817793",
+		"full_text": "After probably the most overwhelming week ever, I took it upon myself to cheer me up so I stayed behind after school and learnt ‘the shuffle.’\n\nNot gonna lie, this did wonders for my wellbeing. 😁\n\nHappy Friday #edutwitter! https://t.co/VuseuGDMGV",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			222
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "edutwitter",
+					"indices": [
+						210,
+						221
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1170039140655804400,
+					"id_str": "1170039140655804416",
+					"indices": [
+						223,
+						246
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1170039140655804416/pu/img/Htcj-CxqpqX_Be_f.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1170039140655804416/pu/img/Htcj-CxqpqX_Be_f.jpg",
+					"url": "https://t.co/VuseuGDMGV",
+					"display_url": "pic.twitter.com/VuseuGDMGV",
+					"expanded_url": "https://twitter.com/UnofficialOA/status/1170039261212680192/video/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1280,
+							"h": 720,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1170039140655804400,
+					"id_str": "1170039140655804416",
+					"indices": [
+						223,
+						246
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1170039140655804416/pu/img/Htcj-CxqpqX_Be_f.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1170039140655804416/pu/img/Htcj-CxqpqX_Be_f.jpg",
+					"url": "https://t.co/VuseuGDMGV",
+					"display_url": "pic.twitter.com/VuseuGDMGV",
+					"expanded_url": "https://twitter.com/UnofficialOA/status/1170039261212680192/video/1",
+					"type": "video",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1280,
+							"h": 720,
+							"resize": "fit"
+						}
+					},
+					"video_info": {
+						"aspect_ratio": [
+							16,
+							9
+						],
+						"duration_millis": 23785,
+						"variants": [
+							{
+								"content_type": "application/x-mpegURL",
+								"url": "https://video.twimg.com/ext_tw_video/1170039140655804416/pu/pl/hr4zC32Hcvi9z-Lp.m3u8?tag=10"
+							},
+							{
+								"bitrate": 832000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1170039140655804416/pu/vid/640x360/xOjBXd26GbS891fY.mp4?tag=10"
+							},
+							{
+								"bitrate": 256000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1170039140655804416/pu/vid/480x270/fHg2vN81lih258n8.mp4?tag=10"
+							},
+							{
+								"bitrate": 2176000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1170039140655804416/pu/vid/1280x720/4U6R9sf1ENXn_2fF.mp4?tag=10"
+							}
+						]
+					},
+					"additional_media_info": {
+						"monetizable": false
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 869159767838740500,
+			"id_str": "869159767838740480",
+			"name": "Teacher's Manual 🙋🏻‍♂️",
+			"screen_name": "UnofficialOA",
+			"location": "Birmingham, UK",
+			"description": "Omar Akbar; Teacher, Author- The Unofficial Teacher's Manual: What they don't teach you in training; Bad School Leadership (and what to do about it) On Amazon.",
+			"url": "https://t.co/D2jvSVxSLo",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/D2jvSVxSLo",
+							"expanded_url": "https://theunofficialteachersmanual.blog",
+							"display_url": "theunofficialteachersmanual.blog",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 7612,
+			"friends_count": 2151,
+			"listed_count": 16,
+			"created_at": "Mon May 29 11:53:31 +0000 2017",
+			"favourites_count": 13506,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 6614,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1144288518103031808/UWw62oA6_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1144288518103031808/UWw62oA6_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/869159767838740480/1507359846",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 2011,
+		"favorite_count": 20367,
+		"favorited": true,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "After probably the most overwhelming week ever, I took it upon myself to cheer me up so I stayed behind after school and learnt ‘the shuffle.’\n\nNot gonna lie, this did wonders for my wellbeing. 😁\n\nHappy Friday #edutwitter! https://t.co/VuseuGDMGV"
+	},
+	{
 		"created_at": "Thu Sep 05 07:45:27 +0000 2019",
 		"id": 1169495102819057700,
 		"id_str": "1169516881490980864",

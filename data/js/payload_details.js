345255dce64481bacc7d207b0e2d6136c67b241d
LeeFallin <lee@newgecko.net>
2019-09-23 01:21:17 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 9002,
-	"created_at": "Sat Sep 21 00:21:16 +0000 2019",
+	"tweets": 9003,
+	"created_at": "Mon Sep 23 00:21:16 +0000 2019",
 	"lang": "en-gb"
 }
--- data/js/tweets/2019_09.js
+++ data/js/tweets/2019_09.js
@@ -1,6 +1,120 @@
 Grailbird.data.tweets_2019_09 = 
 [
 	{
+		"created_at": "Sun Sep 22 07:07:06 +0000 2019",
+		"id": 1174949785142317000,
+		"id_str": "1175667824322367488",
+		"full_text": "The signs on show at the climate strikes were smart, funny and to the point. #ClimateStrike https://t.co/nmXJGdYpuA",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			115
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "ClimateStrike",
+					"indices": [
+						77,
+						91
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/nmXJGdYpuA",
+					"expanded_url": "https://twitter.com/i/events/1174942728276201473",
+					"display_url": "twitter.com/i/events/11749…",
+					"indices": [
+						92,
+						115
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 3260518932,
+			"id_str": "3260518932",
+			"name": "Twitter Moments",
+			"screen_name": "TwitterMoments",
+			"location": "New York, USA",
+			"description": "The best of what’s happening on Twitter in an instant.",
+			"url": "https://t.co/nYOx6qThjk",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/nYOx6qThjk",
+							"expanded_url": "https://about.twitter.com/company/moments-guidelines",
+							"display_url": "about.twitter.com/company/moment…",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 736341,
+			"friends_count": 10,
+			"listed_count": 4567,
+			"created_at": "Tue Jun 30 01:06:59 +0000 2015",
+			"favourites_count": 158,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 66071,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/651463624330907648/OzaAcuSR_normal.png",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/651463624330907648/OzaAcuSR_normal.png",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3260518932/1444135144",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "regular"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 179,
+		"favorite_count": 749,
+		"favorited": true,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "The signs on show at the climate strikes were smart, funny and to the point. #ClimateStrike https://t.co/nmXJGdYpuA"
+	},
+	{
 		"created_at": "Fri Sep 20 12:47:21 +0000 2019",
 		"id": 1175028674569134000,
 		"id_str": "1175028674569134081",
@@ -119,7 +233,7 @@ Grailbird.data.tweets_2019_09 =
 			"name": "Lee Fallin",
 			"screen_name": "LeeFallin",
 			"location": "United Kingdom",
-			"description": "SFHEA #LearningDeveloper, #EdD student, #MCE & #MIEExpert @UniofHull. I tweet #HigherEd, #EdTech, #Lego, #LDchat & #Hull. All tweets in a personal capacity 🏳️‍🌈",
+			"description": "SFHEA #LearningDeveloper, #EdD student, #MCE & #MIEExpert @UniofHull. I tweet #HigherEd, #EdTech, #Lego, #LDchat & #Hull. All tweets in a personal capacity 🏳️🌈",
 			"url": "https://t.co/UWrodKaQLd",
 			"entities": {
 				"url": {

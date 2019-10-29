65ba28dd5203c220ad4bb30e48d7b23108c77124
LeeFallin <lee@newgecko.net>
2019-10-29 00:21:19 +0000

    Added 1 tweets

--- data/js/tweets/2019_10.js
+++ data/js/tweets/2019_10.js
@@ -1,6 +1,141 @@
 Grailbird.data.tweets_2019_10 = 
 [
 	{
+		"created_at": "Mon Oct 28 22:44:34 +0000 2019",
+		"id": 1188792303806308400,
+		"id_str": "1188949707516760066",
+		"full_text": "Using #LegoSeriousPlay at universities. @chrissinerantzi  and @alisonrjames created this useful booklet that you can download for free: https://t.co/JImFhZFURI",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			159
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "LegoSeriousPlay",
+					"indices": [
+						6,
+						22
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "chrissinerantzi",
+					"name": "Chrissi Nerantzi 🇬🇷🇪🇺🐝🇬🇧",
+					"id": 34362372,
+					"id_str": "34362372",
+					"indices": [
+						40,
+						56
+					]
+				},
+				{
+					"screen_name": "alisonrjames",
+					"name": "Prof Alison James",
+					"id": 143180609,
+					"id_str": "143180609",
+					"indices": [
+						62,
+						75
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/JImFhZFURI",
+					"expanded_url": "https://seriousplaypro.com/2019/10/21/using-lego-serious-play-at-universities/",
+					"display_url": "seriousplaypro.com/2019/10/21/usi…",
+					"indices": [
+						136,
+						159
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 150161605,
+			"id_str": "150161605",
+			"name": "Serious Play Pro",
+			"screen_name": "seriousplaypro",
+			"location": "Global",
+			"description": "#Global #Community of #LEGOSERIOUSPLAY #Facilitators. Managed by @MarkoRillo  #Facilitation #Strategy, #LEGO #SERIOUSPLAY Contact: marko@seriousplaypro.com",
+			"url": "http://t.co/pDFL7yDszS",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "http://t.co/pDFL7yDszS",
+							"expanded_url": "http://seriousplaypro.com",
+							"display_url": "seriousplaypro.com",
+							"indices": [
+								0,
+								22
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 1476,
+			"friends_count": 290,
+			"listed_count": 61,
+			"created_at": "Mon May 31 07:07:19 +0000 2010",
+			"favourites_count": 67,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 531,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/893065998709923840/nDAA-tKm_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/893065998709923840/nDAA-tKm_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/150161605/1501757592",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 5,
+		"favorite_count": 18,
+		"favorited": true,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Using #LegoSeriousPlay at universities. @chrissinerantzi  and @alisonrjames created this useful booklet that you can download for free: https://t.co/JImFhZFURI"
+	},
+	{
 		"created_at": "Sun Oct 27 09:48:01 +0000 2019",
 		"id": 1188391896231633000,
 		"id_str": "1188391896231632896",
@@ -36,7 +171,7 @@ Grailbird.data.tweets_2019_10 =
 			"name": "Lee Fallin",
 			"screen_name": "LeeFallin",
 			"location": "United Kingdom",
-			"description": "SFHEA #LearningDeveloper, #EdD student, #MCE & #MIEExpert @UniofHull. I tweet #HigherEd, #EdTech, #Lego, #LDchat & #Hull. All tweets in a personal capacity 🏳️‍🌈",
+			"description": "SFHEA #LearningDeveloper, #EdD student, #MCE & #MIEExpert @UniofHull. I tweet #HigherEd, #EdTech, #Lego, #LDchat & #Hull. All tweets in a personal capacity 🏳️🌈",
 			"url": "https://t.co/UWrodKaQLd",
 			"entities": {
 				"url": {

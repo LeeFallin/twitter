70163724c95191c2fa4b1c40aff89af9cee22b1f
LeeFallin <lee@newgecko.net>
2019-12-31 00:21:18 +0000

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 9720,
-	"created_at": "Mon Dec 30 00:21:17 +0000 2019",
+	"tweets": 9721,
+	"created_at": "Tue Dec 31 00:21:17 +0000 2019",
 	"lang": "en-gb"
 }
--- data/js/tweets/2019_12.js
+++ data/js/tweets/2019_12.js
@@ -1,10 +1,275 @@
 Grailbird.data.tweets_2019_12 = 
 [
 	{
+		"created_at": "Mon Dec 30 10:24:58 +0000 2019",
+		"id": 1211593628516266000,
+		"id_str": "1211594016502013952",
+		"full_text": "Pre drinks before flying to dublin #cheeky breakfast ⁦@LeeFallin⁩ https://t.co/78br63ZkMF",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			65
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "cheeky",
+					"indices": [
+						35,
+						42
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "LeeFallin",
+					"name": "Lee Fallin",
+					"id": 55448837,
+					"id_str": "55448837",
+					"indices": [
+						54,
+						64
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 1211593559595475000,
+					"id_str": "1211593559595474944",
+					"indices": [
+						66,
+						89
+					],
+					"media_url": "http://pbs.twimg.com/media/ENBx7FPW4AAFv2t.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/ENBx7FPW4AAFv2t.jpg",
+					"url": "https://t.co/78br63ZkMF",
+					"display_url": "pic.twitter.com/78br63ZkMF",
+					"expanded_url": "https://twitter.com/kevfallin/status/1211593628516265986/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1152,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						}
+					},
+					"features": {
+						"medium": {
+							"faces": [
+								{
+									"x": 533,
+									"y": 419,
+									"h": 64,
+									"w": 64
+								}
+							]
+						},
+						"large": {
+							"faces": [
+								{
+									"x": 910,
+									"y": 716,
+									"h": 110,
+									"w": 110
+								}
+							]
+						},
+						"orig": {
+							"faces": [
+								{
+									"x": 910,
+									"y": 716,
+									"h": 110,
+									"w": 110
+								}
+							]
+						},
+						"small": {
+							"faces": [
+								{
+									"x": 302,
+									"y": 237,
+									"h": 36,
+									"w": 36
+								}
+							]
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1211593559595475000,
+					"id_str": "1211593559595474944",
+					"indices": [
+						66,
+						89
+					],
+					"media_url": "http://pbs.twimg.com/media/ENBx7FPW4AAFv2t.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/ENBx7FPW4AAFv2t.jpg",
+					"url": "https://t.co/78br63ZkMF",
+					"display_url": "pic.twitter.com/78br63ZkMF",
+					"expanded_url": "https://twitter.com/kevfallin/status/1211593628516265986/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1152,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						}
+					},
+					"features": {
+						"medium": {
+							"faces": [
+								{
+									"x": 533,
+									"y": 419,
+									"h": 64,
+									"w": 64
+								}
+							]
+						},
+						"large": {
+							"faces": [
+								{
+									"x": 910,
+									"y": 716,
+									"h": 110,
+									"w": 110
+								}
+							]
+						},
+						"orig": {
+							"faces": [
+								{
+									"x": 910,
+									"y": 716,
+									"h": 110,
+									"w": 110
+								}
+							]
+						},
+						"small": {
+							"faces": [
+								{
+									"x": 302,
+									"y": 237,
+									"h": 36,
+									"w": 36
+								}
+							]
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1174207229286912000,
+			"id_str": "1174207229286912000",
+			"name": "Kevfallin",
+			"screen_name": "kevfallin",
+			"location": "Hull, England",
+			"description": "",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 13,
+			"friends_count": 221,
+			"listed_count": 0,
+			"created_at": "Wed Sep 18 06:23:26 +0000 2019",
+			"favourites_count": 477,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 28,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1174207337323798529/53Dd10vW_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1174207337323798529/53Dd10vW_normal.jpg",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": true,
+			"default_profile_image": false,
+			"can_media_tag": true,
+			"followed_by": true,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1,
+		"favorite_count": 9,
+		"favorited": true,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Pre drinks before flying to dublin #cheeky breakfast ⁦@LeeFallin⁩ https://t.co/78br63ZkMF"
+	},
+	{
 		"created_at": "Sun Dec 29 18:42:03 +0000 2019",
 		"id": 1211356725409919000,
 		"id_str": "1211356725409918976",
-		"full_text": "New Years plan in emojis: \n\n👬✈️🇮🇪🍾🍻☘️🎊🕛👨‍❤️‍💋‍👨🎆🎇🌈🎩🍀",
+		"full_text": "New Years plan in emojis: \n\n👬✈️🇮🇪🍾🍻☘️🎊🕛👨❤️💋👨🎆🎇🌈🎩🍀",
 		"truncated": false,
 		"display_text_range": [
 			0,
@@ -28,7 +293,7 @@ Grailbird.data.tweets_2019_12 =
 			"name": "Lee Fallin",
 			"screen_name": "LeeFallin",
 			"location": "United Kingdom",
-			"description": "SFHEA #LearningDeveloper, #EdD student, #MCE & #MIEExpert @UniofHull. I tweet #HigherEd, #EdTech, #Lego, #LDchat & #Hull. All tweets in a personal capacity 🏳️‍🌈",
+			"description": "SFHEA #LearningDeveloper, #EdD student, #MCE & #MIEExpert @UniofHull. I tweet #HigherEd, #EdTech, #Lego, #LDchat & #Hull. All tweets in a personal capacity 🏳️🌈",
 			"url": "https://t.co/UWrodKaQLd",
 			"entities": {
 				"url": {
@@ -95,7 +360,7 @@ Grailbird.data.tweets_2019_12 =
 		"favorited": false,
 		"retweeted": false,
 		"lang": "en",
-		"text": "New Years plan in emojis: \n\n👬✈️🇮🇪🍾🍻☘️🎊🕛👨‍❤️‍💋‍👨🎆🎇🌈🎩🍀"
+		"text": "New Years plan in emojis: \n\n👬✈️🇮🇪🍾🍻☘️🎊🕛👨❤️💋👨🎆🎇🌈🎩🍀"
 	},
 	{
 		"created_at": "Sun Dec 29 18:38:29 +0000 2019",
@@ -125,7 +390,7 @@ Grailbird.data.tweets_2019_12 =
 			"name": "Lee Fallin",
 			"screen_name": "LeeFallin",
 			"location": "United Kingdom",
-			"description": "SFHEA #LearningDeveloper, #EdD student, #MCE & #MIEExpert @UniofHull. I tweet #HigherEd, #EdTech, #Lego, #LDchat & #Hull. All tweets in a personal capacity 🏳️‍🌈",
+			"description": "SFHEA #LearningDeveloper, #EdD student, #MCE & #MIEExpert @UniofHull. I tweet #HigherEd, #EdTech, #Lego, #LDchat & #Hull. All tweets in a personal capacity 🏳️🌈",
 			"url": "https://t.co/UWrodKaQLd",
 			"entities": {
 				"url": {

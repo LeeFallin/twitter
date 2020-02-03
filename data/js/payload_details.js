3b86e73e0e91d800be274f2c880c6d554cafc8ac
LeeFallin <lee@newgecko.net>
2020-02-03 00:21:17 +0000

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 9819,
-	"created_at": "Fri Jan 31 00:21:18 +0000 2020",
+	"tweets": 9820,
+	"created_at": "Mon Feb 03 00:21:16 +0000 2020",
 	"lang": "en-gb"
 }
---
+++ data/js/tweets/2020_02.js
@@ -0,0 +1,98 @@
+Grailbird.data.tweets_2020_02 = 
+[
+	{
+		"created_at": "Sun Feb 02 10:03:49 +0000 2020",
+		"id": 1223704182567272400,
+		"id_str": "1223909879846449153",
+		"full_text": "Why Microsoft and Tesla are the decade’s big disrupters https://t.co/k20Ykv1Ekp",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			79
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/k20Ykv1Ekp",
+					"expanded_url": "https://lnkd.in/gNXsZGJ",
+					"display_url": "lnkd.in/gNXsZGJ",
+					"indices": [
+						56,
+						79
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://www.linkedin.com/\" rel=\"nofollow\">LinkedIn</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 428257653,
+			"id_str": "428257653",
+			"name": "Clare Riley",
+			"screen_name": "ClareRiley100",
+			"location": "",
+			"description": "FE and HE engagement manager for Microsoft in the UK",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 1277,
+			"friends_count": 229,
+			"listed_count": 12,
+			"created_at": "Sun Dec 04 14:55:33 +0000 2011",
+			"favourites_count": 66,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 1137,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/977202405451485185/ul58W5np_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/977202405451485185/ul58W5np_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/428257653/1555312501",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"can_media_tag": true,
+			"followed_by": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 2,
+		"favorite_count": 6,
+		"favorited": true,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Why Microsoft and Tesla are the decade’s big disrupters https://t.co/k20Ykv1Ekp"
+	}
+]

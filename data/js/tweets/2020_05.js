4d609159fb878d5c1527c209cae220993e43d131
LeeFallin <lee@newgecko.net>
2020-05-31 01:21:17 +0100

    Added 1 tweets

--- data/js/tweets/2020_05.js
+++ data/js/tweets/2020_05.js
@@ -1,6 +1,111 @@
 Grailbird.data.tweets_2020_05 = 
 [
 	{
+		"created_at": "Sat May 30 07:29:56 +0000 2020",
+		"id": 1266287673766850600,
+		"id_str": "1266632922267779072",
+		"full_text": "Working on that literature review? Our very popular Three Domains of Critical Reading resource is now available as an interactive resource with audio as well as the printable guide! https://t.co/MMRgJ7sZHG",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			205
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/MMRgJ7sZHG",
+					"expanded_url": "https://internal.ncl.ac.uk/ask/dissertations-theses-research-projects",
+					"display_url": "internal.ncl.ac.uk/ask/dissertati…",
+					"indices": [
+						182,
+						205
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 2969557323,
+			"id_str": "2969557323",
+			"name": "Writing Development",
+			"screen_name": "NCL_WDC",
+			"location": "Newcastle on Tyne, UK",
+			"description": "Writing Development Centre, Newcastle University. Advice on academic writing and successful study strategies",
+			"url": "http://t.co/QeSl9a1cDc",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "http://t.co/QeSl9a1cDc",
+							"expanded_url": "http://www.ncl.ac.uk/students/wdc/",
+							"display_url": "ncl.ac.uk/students/wdc/",
+							"indices": [
+								0,
+								22
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 440,
+			"friends_count": 55,
+			"listed_count": 10,
+			"created_at": "Fri Jan 09 12:05:52 +0000 2015",
+			"favourites_count": 26,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 527,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/553525379072147456/l6ZyJA7k_normal.jpeg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/553525379072147456/l6ZyJA7k_normal.jpeg",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 6,
+		"favorite_count": 15,
+		"favorited": true,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Working on that literature review? Our very popular Three Domains of Critical Reading resource is now available as an interactive resource with audio as well as the printable guide! https://t.co/MMRgJ7sZHG"
+	},
+	{
 		"created_at": "Mon May 25 10:16:33 +0000 2020",
 		"id": 1264608733541384200,
 		"id_str": "1264862915527614471",

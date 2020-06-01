7a5f314b1cbde415ed3bf5d0fab0f4dbf1290e30
LeeFallin <lee@newgecko.net>
2020-06-01 01:21:17 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 10043,
-	"created_at": "Sun May 31 00:21:16 +0000 2020",
+	"tweets": 10044,
+	"created_at": "Mon Jun 01 00:21:16 +0000 2020",
 	"lang": "en-gb"
 }
--- data/js/tweets/2020_05.js
+++ data/js/tweets/2020_05.js
@@ -1,6 +1,99 @@
 Grailbird.data.tweets_2020_05 = 
 [
 	{
+		"created_at": "Sun May 31 08:29:08 +0000 2020",
+		"id": 1266746014796583000,
+		"id_str": "1267010211367305216",
+		"full_text": "How Hull's year as City of Culture helped to provide £80m boost for one of world's greatest port cities https://t.co/K7qx8HjLOL",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			127
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/K7qx8HjLOL",
+					"expanded_url": "https://www.yorkshirepost.co.uk/business/how-hulls-year-city-culture-helped-provide-ps80m-boost-one-worlds-greatest-port-cities-2868774",
+					"display_url": "yorkshirepost.co.uk/business/how-h…",
+					"indices": [
+						104,
+						127
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/#!/download/ipad\" rel=\"nofollow\">Twitter for iPad</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 632179140,
+			"id_str": "632179140",
+			"name": "Jane Gibson",
+			"screen_name": "joinedupnorth",
+			"location": "",
+			"description": "Third sector non exec lives in York used to be Blackburn @spiritof2012 • Hull • Hadrian's Wall • @bizinspiredgrow",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 1203,
+			"friends_count": 1477,
+			"listed_count": 10,
+			"created_at": "Tue Jul 10 16:04:11 +0000 2012",
+			"favourites_count": 3652,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 3785,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1065692844688637952/Ig6ynHwG_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1065692844688637952/Ig6ynHwG_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/632179140/1541700231",
+			"profile_link_color": "1B95E0",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 10,
+		"favorite_count": 26,
+		"favorited": true,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "How Hull's year as City of Culture helped to provide £80m boost for one of world's greatest port cities https://t.co/K7qx8HjLOL"
+	},
+	{
 		"created_at": "Sat May 30 07:29:56 +0000 2020",
 		"id": 1266287673766850600,
 		"id_str": "1266632922267779072",

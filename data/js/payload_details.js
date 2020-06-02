136eb83e3cff844c52a6357880b67c97fe48fcfd
LeeFallin <lee@newgecko.net>
2020-06-02 01:21:17 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 10044,
-	"created_at": "Mon Jun 01 00:21:16 +0000 2020",
+	"tweets": 10045,
+	"created_at": "Tue Jun 02 00:21:16 +0000 2020",
 	"lang": "en-gb"
 }

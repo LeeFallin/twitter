58608e7699cddfa1cd4ff1206476aec9fd90f826
LeeFallin <lee@newgecko.net>
2020-06-11 01:21:17 +0100

    Added 2 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 10046,
-	"created_at": "Fri Jun 05 00:21:15 +0000 2020",
+	"tweets": 10048,
+	"created_at": "Thu Jun 11 00:21:15 +0000 2020",
 	"lang": "en-gb"
 }
--- data/js/tweets/2020_06.js
+++ data/js/tweets/2020_06.js
@@ -1,6 +1,438 @@
 Grailbird.data.tweets_2020_06 = 
 [
 	{
+		"created_at": "Wed Jun 10 16:31:41 +0000 2020",
+		"id": 1270741510313697300,
+		"id_str": "1270755526104809474",
+		"full_text": "Queries, questions, text or face to face, listen in or chit-chat. Happening tomorrow Thurs 11 June from 4-5pm. Book here &amp; we'll send you the joining link on the day: https://t.co/tiIhc2juoV \nShare with your PGRs! @FBLP_research @FSEFacultyRese1 @FacHealthHull @UoHEducation https://t.co/li0gjHrAwq",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			278
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "FBLP_research",
+					"name": "FBLP Research",
+					"id": 840208138078949400,
+					"id_str": "840208138078949377",
+					"indices": [
+						218,
+						232
+					]
+				},
+				{
+					"screen_name": "FSEFacultyRese1",
+					"name": "FSE_Research",
+					"id": 1133768144315736000,
+					"id_str": "1133768144315736064",
+					"indices": [
+						233,
+						249
+					]
+				},
+				{
+					"screen_name": "FacHealthHull",
+					"name": "Hull Health Sciences",
+					"id": 971909150,
+					"id_str": "971909150",
+					"indices": [
+						250,
+						264
+					]
+				},
+				{
+					"screen_name": "UoHEducation",
+					"name": "University of Hull, School Of Education",
+					"id": 1224258056021778400,
+					"id_str": "1224258056021778433",
+					"indices": [
+						265,
+						278
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/tiIhc2juoV",
+					"expanded_url": "https://libcal.hull.ac.uk/event/3461859",
+					"display_url": "libcal.hull.ac.uk/event/3461859",
+					"indices": [
+						171,
+						194
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1270740930153365500,
+					"id_str": "1270740930153365504",
+					"indices": [
+						279,
+						302
+					],
+					"media_url": "http://pbs.twimg.com/media/EaKUJK9XQAAEFY8.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EaKUJK9XQAAEFY8.jpg",
+					"url": "https://t.co/li0gjHrAwq",
+					"display_url": "pic.twitter.com/li0gjHrAwq",
+					"expanded_url": "https://twitter.com/pgrhub/status/1270741510313697283/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 471,
+							"h": 471,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 471,
+							"h": 471,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 471,
+							"h": 471,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1270740930153365500,
+					"id_str": "1270740930153365504",
+					"indices": [
+						279,
+						302
+					],
+					"media_url": "http://pbs.twimg.com/media/EaKUJK9XQAAEFY8.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EaKUJK9XQAAEFY8.jpg",
+					"url": "https://t.co/li0gjHrAwq",
+					"display_url": "pic.twitter.com/li0gjHrAwq",
+					"expanded_url": "https://twitter.com/pgrhub/status/1270741510313697283/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 471,
+							"h": 471,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 471,
+							"h": 471,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 471,
+							"h": 471,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 741579412295012400,
+			"id_str": "741579412295012352",
+			"name": "Doctoral College @ the University of Hull",
+			"screen_name": "pgrhub",
+			"location": "Kingston Upon Hull",
+			"description": "The Doctoral College Team @UniOfHull. Developing & supporting PGR students & PGR supervisors in pursuit of research excellence. E:Doctoralcollege@hull.ac.uk",
+			"url": "https://t.co/0Uup08l5SD",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/0Uup08l5SD",
+							"expanded_url": "https://www.hull.ac.uk/choose-hull/study-at-hull/doctoral-college/doctoral-college.aspx",
+							"display_url": "hull.ac.uk/choose-hull/st…",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 910,
+			"friends_count": 844,
+			"listed_count": 5,
+			"created_at": "Sat Jun 11 10:34:44 +0000 2016",
+			"favourites_count": 2168,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 2822,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1133780651143061504/1dOMNjai_normal.png",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1133780651143061504/1dOMNjai_normal.png",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/741579412295012352/1524836696",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 19,
+		"favorite_count": 15,
+		"favorited": true,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Queries, questions, text or face to face, listen in or chit-chat. Happening tomorrow Thurs 11 June from 4-5pm. Book here &amp; we'll send you the joining link on the day: https://t.co/tiIhc2juoV \nShare with your PGRs! @FBLP_research @FSEFacultyRese1 @FacHealthHull @UoHEducation https://t.co/li0gjHrAwq"
+	},
+	{
+		"created_at": "Wed Jun 10 08:37:29 +0000 2020",
+		"id": 1270636189176017000,
+		"id_str": "1270636189176016896",
+		"full_text": "@Carolyn28002033 @UniOfHull @HullNursing @spiderlegsmooni @livinginhope @JacquieWhite7 @AndyPaulBarker @timeshighered @b33f4rt Look forward to it Carolyn and welcome to the @UniOfHull 😁\nhttps://t.co/AIjeSmqcHz",
+		"truncated": false,
+		"display_text_range": [
+			127,
+			209
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "Carolyn28002033",
+					"name": "Carolyn",
+					"id": 1267136816056012800,
+					"id_str": "1267136816056012800",
+					"indices": [
+						0,
+						16
+					]
+				},
+				{
+					"screen_name": "UniOfHull",
+					"name": "University of Hull",
+					"id": 59761280,
+					"id_str": "59761280",
+					"indices": [
+						17,
+						27
+					]
+				},
+				{
+					"screen_name": "HullNursing",
+					"name": "Uni of Hull Nursing",
+					"id": 2644622095,
+					"id_str": "2644622095",
+					"indices": [
+						28,
+						40
+					]
+				},
+				{
+					"screen_name": "spiderlegsmooni",
+					"name": "Kirsty E. Fishburn",
+					"id": 78066126,
+					"id_str": "78066126",
+					"indices": [
+						41,
+						57
+					]
+				},
+				{
+					"screen_name": "livinginhope",
+					"name": "Dr Jane Wray",
+					"id": 22756431,
+					"id_str": "22756431",
+					"indices": [
+						58,
+						71
+					]
+				},
+				{
+					"screen_name": "JacquieWhite7",
+					"name": "Dr Jacquie White",
+					"id": 747518964889169900,
+					"id_str": "747518964889169920",
+					"indices": [
+						72,
+						86
+					]
+				},
+				{
+					"screen_name": "AndyPaulBarker",
+					"name": "Andy",
+					"id": 20244386,
+					"id_str": "20244386",
+					"indices": [
+						87,
+						102
+					]
+				},
+				{
+					"screen_name": "timeshighered",
+					"name": "Times Higher Education",
+					"id": 23602600,
+					"id_str": "23602600",
+					"indices": [
+						103,
+						117
+					]
+				},
+				{
+					"screen_name": "b33f4rt",
+					"name": "Tim Büscher",
+					"id": 241314079,
+					"id_str": "241314079",
+					"indices": [
+						118,
+						126
+					]
+				},
+				{
+					"screen_name": "UniOfHull",
+					"name": "University of Hull",
+					"id": 59761280,
+					"id_str": "59761280",
+					"indices": [
+						173,
+						183
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/AIjeSmqcHz",
+					"expanded_url": "https://www.hull.ac.uk/choose-hull/study-at-hull/library/staff",
+					"display_url": "hull.ac.uk/choose-hull/st…",
+					"indices": [
+						186,
+						209
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": 1270624817859920000,
+		"in_reply_to_status_id_str": "1270624817859919878",
+		"in_reply_to_user_id": 1267136816056012800,
+		"in_reply_to_user_id_str": "1267136816056012800",
+		"in_reply_to_screen_name": "Carolyn28002033",
+		"user": {
+			"id": 55448837,
+			"id_str": "55448837",
+			"name": "Lee Fallin",
+			"screen_name": "LeeFallin",
+			"location": "United Kingdom",
+			"description": "SFHEA #LearningDeveloper, #EdD student, #MCE & #MIEExpert @UniofHull. I tweet #HigherEd, #EdTech, #Lego, #LDchat & #Hull. All tweets in a personal capacity 🏳️‍🌈",
+			"url": "https://t.co/UWrodKaQLd",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/UWrodKaQLd",
+							"expanded_url": "http://www.leefallin.co.uk",
+							"display_url": "leefallin.co.uk",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 1349,
+			"friends_count": 1170,
+			"listed_count": 62,
+			"created_at": "Fri Jul 10 02:53:40 +0000 2009",
+			"favourites_count": 6754,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 10140,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/753894560108011520/7h68mawt_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/753894560108011520/7h68mawt_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/55448837/1555627842",
+			"profile_link_color": "1B95E0",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 2,
+		"favorite_count": 4,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "@Carolyn28002033 @UniOfHull @HullNursing @spiderlegsmooni @livinginhope @JacquieWhite7 @AndyPaulBarker @timeshighered @b33f4rt Look forward to it Carolyn and welcome to the @UniOfHull 😁\nhttps://t.co/AIjeSmqcHz"
+	},
+	{
 		"created_at": "Thu Jun 04 15:48:07 +0000 2020",
 		"id": 1268570233876422700,
 		"id_str": "1268570233876422656",
@@ -49,7 +481,7 @@ Grailbird.data.tweets_2020_06 =
 			"name": "Lee Fallin",
 			"screen_name": "LeeFallin",
 			"location": "United Kingdom",
-			"description": "SFHEA #LearningDeveloper, #EdD student, #MCE & #MIEExpert @UniofHull. I tweet #HigherEd, #EdTech, #Lego, #LDchat & #Hull. All tweets in a personal capacity 🏳️‍🌈",
+			"description": "SFHEA #LearningDeveloper, #EdD student, #MCE & #MIEExpert @UniofHull. I tweet #HigherEd, #EdTech, #Lego, #LDchat & #Hull. All tweets in a personal capacity 🏳️🌈",
 			"url": "https://t.co/UWrodKaQLd",
 			"entities": {
 				"url": {

4b14979aa9232280434dfc1e9f42ba6979b2bdac
LeeFallin <lee@newgecko.net>
2020-07-02 01:21:18 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 10090,
-	"created_at": "Wed Jul 01 00:21:16 +0000 2020",
+	"tweets": 10091,
+	"created_at": "Thu Jul 02 00:21:16 +0000 2020",
 	"lang": "en-gb"
 }
---
+++ data/js/tweets/2020_07.js
@@ -0,0 +1,120 @@
+Grailbird.data.tweets_2020_07 = 
+[
+	{
+		"created_at": "Wed Jul 01 16:23:14 +0000 2020",
+		"id": 1278015704122433500,
+		"id_str": "1278363545151963137",
+		"full_text": ".@NASA blows us away once again. https://t.co/uTQOk1E8VI",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			56
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "NASA",
+					"name": "NASA",
+					"id": 11348282,
+					"id_str": "11348282",
+					"indices": [
+						1,
+						6
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/uTQOk1E8VI",
+					"expanded_url": "https://www.thrillist.com/news/nation/nasa-sun-time-lapse-10-years-video?utm_source=twitter&utm_medium=social&utm_campaign=thrillist",
+					"display_url": "thrillist.com/news/nation/na…",
+					"indices": [
+						33,
+						56
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 16402507,
+			"id_str": "16402507",
+			"name": "Thrillist",
+			"screen_name": "Thrillist",
+			"location": "",
+			"description": "Taking fun seriously.",
+			"url": "https://t.co/mLPcsvzn4O",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/mLPcsvzn4O",
+							"expanded_url": "http://thrl.st/RWCF-Donate",
+							"display_url": "thrl.st/RWCF-Donate",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 129953,
+			"friends_count": 3701,
+			"listed_count": 2747,
+			"created_at": "Mon Sep 22 12:19:06 +0000 2008",
+			"favourites_count": 11156,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 48848,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "808080",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1272359691151122434/JeOic197_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1272359691151122434/JeOic197_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/16402507/1587388249",
+			"profile_link_color": "0084B4",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "FFFFFF",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 3927,
+		"favorite_count": 10496,
+		"favorited": true,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": ".@NASA blows us away once again. https://t.co/uTQOk1E8VI"
+	}
+]

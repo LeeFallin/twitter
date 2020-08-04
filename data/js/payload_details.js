251d13db6a0758cbee5944efd341deac64a430b8
LeeFallin <lee@newgecko.net>
2020-08-04 01:21:18 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 10111,
-	"created_at": "Sun Aug 02 00:21:16 +0000 2020",
+	"tweets": 10112,
+	"created_at": "Tue Aug 04 00:21:16 +0000 2020",
 	"lang": "en-gb"
 }
--- data/js/tweets/2020_08.js
+++ data/js/tweets/2020_08.js
@@ -1,6 +1,226 @@
 Grailbird.data.tweets_2020_08 = 
 [
 	{
+		"created_at": "Mon Aug 03 11:11:24 +0000 2020",
+		"id": 1290163511952375800,
+		"id_str": "1290243866696642560",
+		"full_text": "Continuing our exploration of #StreetArt in Hull, this piece of art welcomes visitors arriving at @ABPHumber. It was inspired by the drawings of pupils at @VictoriaDockPS. @UniOfHull #Hull https://t.co/zL8dUgQ1fA",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			188
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "StreetArt",
+					"indices": [
+						30,
+						40
+					]
+				},
+				{
+					"text": "Hull",
+					"indices": [
+						183,
+						188
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "ABPHumber",
+					"name": "ABP Humber",
+					"id": 2615270962,
+					"id_str": "2615270962",
+					"indices": [
+						98,
+						108
+					]
+				},
+				{
+					"screen_name": "VictoriaDockPS",
+					"name": "VictoriaDockPS",
+					"id": 413424096,
+					"id_str": "413424096",
+					"indices": [
+						155,
+						170
+					]
+				},
+				{
+					"screen_name": "UniOfHull",
+					"name": "University of Hull",
+					"id": 59761280,
+					"id_str": "59761280",
+					"indices": [
+						172,
+						182
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 1290160422365077500,
+					"id_str": "1290160422365077509",
+					"indices": [
+						189,
+						212
+					],
+					"media_url": "http://pbs.twimg.com/media/EeeSEY5WoAUKahu.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EeeSEY5WoAUKahu.jpg",
+					"url": "https://t.co/zL8dUgQ1fA",
+					"display_url": "pic.twitter.com/zL8dUgQ1fA",
+					"expanded_url": "https://twitter.com/HistoryatHull/status/1290163511952375809/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 698,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1191,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 396,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1290160422365077500,
+					"id_str": "1290160422365077509",
+					"indices": [
+						189,
+						212
+					],
+					"media_url": "http://pbs.twimg.com/media/EeeSEY5WoAUKahu.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EeeSEY5WoAUKahu.jpg",
+					"url": "https://t.co/zL8dUgQ1fA",
+					"display_url": "pic.twitter.com/zL8dUgQ1fA",
+					"expanded_url": "https://twitter.com/HistoryatHull/status/1290163511952375809/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 698,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1191,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 396,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 308530453,
+			"id_str": "308530453",
+			"name": "History@Hull",
+			"screen_name": "HistoryatHull",
+			"location": "Hull, United Kingdom",
+			"description": "News and events from the Department History at the @UniofHull. We’re a leading centre for historical research and innovative teaching. Study with us!",
+			"url": "https://t.co/kU5acIALyR",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/kU5acIALyR",
+							"expanded_url": "https://www.hull.ac.uk/faculties/subjects/history.aspx",
+							"display_url": "hull.ac.uk/faculties/subj…",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 2200,
+			"friends_count": 1227,
+			"listed_count": 30,
+			"created_at": "Tue May 31 15:28:54 +0000 2011",
+			"favourites_count": 1470,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 4240,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme9/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme9/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1152177753606819841/bSbYUaUL_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1152177753606819841/bSbYUaUL_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/308530453/1572618350",
+			"profile_link_color": "1B95E0",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 4,
+		"favorite_count": 15,
+		"favorited": true,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Continuing our exploration of #StreetArt in Hull, this piece of art welcomes visitors arriving at @ABPHumber. It was inspired by the drawings of pupils at @VictoriaDockPS. @UniOfHull #Hull https://t.co/zL8dUgQ1fA"
+	},
+	{
 		"created_at": "Sat Aug 01 20:07:14 +0000 2020",
 		"id": 1289653939479011300,
 		"id_str": "1289653939479011328",
@@ -39,7 +259,7 @@ Grailbird.data.tweets_2020_08 =
 			"name": "Lee Fallin",
 			"screen_name": "LeeFallin",
 			"location": "United Kingdom",
-			"description": "SFHEA #LearningDeveloper, #EdD student, #MCE & #MIEExpert @UniofHull. I tweet #HigherEd, #EdTech, #Lego, #LDchat & #Hull. All tweets in a personal capacity 🏳️‍🌈",
+			"description": "SFHEA #LearningDeveloper, #EdD student, #MCE & #MIEExpert @UniofHull. I tweet #HigherEd, #EdTech, #Lego, #LDchat & #Hull. All tweets in a personal capacity 🏳️🌈",
 			"url": "https://t.co/UWrodKaQLd",
 			"entities": {
 				"url": {

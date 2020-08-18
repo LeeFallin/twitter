84c3af74c7e3b90dcbfa36259a589d83fbe9f1e4
LeeFallin <lee@newgecko.net>
2020-08-18 01:21:18 +0100

    Added 1 tweets

--- data/js/tweets/2020_08.js
+++ data/js/tweets/2020_08.js
@@ -1,6 +1,150 @@
 Grailbird.data.tweets_2020_08 = 
 [
 	{
+		"created_at": "Mon Aug 17 17:49:41 +0000 2020",
+		"id": 1295413285869813800,
+		"id_str": "1295417531675693056",
+		"full_text": "New paper out! @LeeFallin @DominicCHenri @paulamarshall40 and I contributed to this collection of articles for @AdvanceHE on addressing Hidden Curriculum. See pg 59-77, where we argue empathy with students as individuals is the place to start  https://t.co/RMgcNAhUyQ @UoHAcademy",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			279
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "LeeFallin",
+					"name": "Lee Fallin",
+					"id": 55448837,
+					"id_str": "55448837",
+					"indices": [
+						15,
+						25
+					]
+				},
+				{
+					"screen_name": "DominicCHenri",
+					"name": "Dominic Henri",
+					"id": 1685253343,
+					"id_str": "1685253343",
+					"indices": [
+						26,
+						40
+					]
+				},
+				{
+					"screen_name": "paulamarshall40",
+					"name": "PaulaGawthorpe 💙",
+					"id": 215034684,
+					"id_str": "215034684",
+					"indices": [
+						41,
+						57
+					]
+				},
+				{
+					"screen_name": "AdvanceHE",
+					"name": "Advance HE",
+					"id": 941587234465177600,
+					"id_str": "941587234465177600",
+					"indices": [
+						111,
+						121
+					]
+				},
+				{
+					"screen_name": "UoHAcademy",
+					"name": "Teaching Excellence Academy",
+					"id": 914481006,
+					"id_str": "914481006",
+					"indices": [
+						268,
+						279
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/RMgcNAhUyQ",
+					"expanded_url": "https://www.advance-he.ac.uk/knowledge-hub/hidden-curriculum-higher-education",
+					"display_url": "advance-he.ac.uk/knowledge-hub/…",
+					"indices": [
+						244,
+						267
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 2427419792,
+			"id_str": "2427419792",
+			"name": "Dr Katharine Hubbard",
+			"screen_name": "KEHplantsci",
+			"location": "Hull, UK",
+			"description": "(She/her) Reader in Biological Sciences Education at the University of Hull. National Teaching Fellow, SFHEA. Opinions my own, not of my employer",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 1230,
+			"friends_count": 854,
+			"listed_count": 45,
+			"created_at": "Fri Apr 04 15:03:38 +0000 2014",
+			"favourites_count": 5900,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 3906,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/685469888186548224/yfjZ36zo_normal.png",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/685469888186548224/yfjZ36zo_normal.png",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/2427419792/1428959032",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 8,
+		"favorite_count": 21,
+		"favorited": true,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "New paper out! @LeeFallin @DominicCHenri @paulamarshall40 and I contributed to this collection of articles for @AdvanceHE on addressing Hidden Curriculum. See pg 59-77, where we argue empathy with students as individuals is the place to start  https://t.co/RMgcNAhUyQ @UoHAcademy"
+	},
+	{
 		"created_at": "Thu Aug 13 08:01:29 +0000 2020",
 		"id": 1285242994459848700,
 		"id_str": "1293819954517729281",

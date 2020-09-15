683d8a34633810e1d6191fd8ecf582e6d59c34c8
LeeFallin <lee@newgecko.net>
2020-09-15 01:21:16 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 10154,
-	"created_at": "Sat Sep 12 00:21:15 +0000 2020",
+	"tweets": 10155,
+	"created_at": "Tue Sep 15 00:21:15 +0000 2020",
 	"lang": "en-gb"
 }
--- data/js/tweets/2020_09.js
+++ data/js/tweets/2020_09.js
@@ -1,6 +1,172 @@
 Grailbird.data.tweets_2020_09 = 
 [
 	{
+		"created_at": "Mon Sep 14 12:52:50 +0000 2020",
+		"id": 1305489686954225700,
+		"id_str": "1305489686954225664",
+		"full_text": "@hughjconsulting @AndyPaulBarker @hullalumni @UniOfHull @HullUniUnion @HullUni_Library Does anyone have information on this snap from the past? What building could this be @Hullhistorynews?",
+		"truncated": false,
+		"display_text_range": [
+			87,
+			189
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "hughjconsulting",
+					"name": "Hugh Jones",
+					"id": 2375524495,
+					"id_str": "2375524495",
+					"indices": [
+						0,
+						16
+					]
+				},
+				{
+					"screen_name": "AndyPaulBarker",
+					"name": "Andy",
+					"id": 20244386,
+					"id_str": "20244386",
+					"indices": [
+						17,
+						32
+					]
+				},
+				{
+					"screen_name": "hullalumni",
+					"name": "University of Hull Alumni",
+					"id": 2237655414,
+					"id_str": "2237655414",
+					"indices": [
+						33,
+						44
+					]
+				},
+				{
+					"screen_name": "UniOfHull",
+					"name": "University of Hull",
+					"id": 59761280,
+					"id_str": "59761280",
+					"indices": [
+						45,
+						55
+					]
+				},
+				{
+					"screen_name": "HullUniUnion",
+					"name": "Hull Uni Students' Union",
+					"id": 29240147,
+					"id_str": "29240147",
+					"indices": [
+						56,
+						69
+					]
+				},
+				{
+					"screen_name": "HullUni_Library",
+					"name": "Hull Uni Library",
+					"id": 239865033,
+					"id_str": "239865033",
+					"indices": [
+						70,
+						86
+					]
+				},
+				{
+					"screen_name": "Hullhistorynews",
+					"name": "Hull History Centre",
+					"id": 54942242,
+					"id_str": "54942242",
+					"indices": [
+						172,
+						188
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": 1305477724513935400,
+		"in_reply_to_status_id_str": "1305477724513935360",
+		"in_reply_to_user_id": 2375524495,
+		"in_reply_to_user_id_str": "2375524495",
+		"in_reply_to_screen_name": "hughjconsulting",
+		"user": {
+			"id": 55448837,
+			"id_str": "55448837",
+			"name": "Lee Fallin",
+			"screen_name": "LeeFallin",
+			"location": "United Kingdom",
+			"description": "SFHEA #LearningDeveloper, #EdD student, #MCE & #MIEExpert @UniofHull. I tweet #HigherEd, #EdTech, #Lego, #LDchat & #Hull. All tweets in a personal capacity 🏳️‍🌈",
+			"url": "https://t.co/UWrodKaQLd",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/UWrodKaQLd",
+							"expanded_url": "http://www.leefallin.co.uk",
+							"display_url": "leefallin.co.uk",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 1378,
+			"friends_count": 1205,
+			"listed_count": 62,
+			"created_at": "Fri Jul 10 02:53:40 +0000 2009",
+			"favourites_count": 6991,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 10258,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/753894560108011520/7h68mawt_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/753894560108011520/7h68mawt_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/55448837/1555627842",
+			"profile_link_color": "1B95E0",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "en",
+		"text": "@hughjconsulting @AndyPaulBarker @hullalumni @UniOfHull @HullUniUnion @HullUni_Library Does anyone have information on this snap from the past? What building could this be @Hullhistorynews?"
+	},
+	{
 		"created_at": "Fri Sep 11 06:07:43 +0000 2020",
 		"id": 1304300572611408000,
 		"id_str": "1304300572611407872",
@@ -38,7 +204,7 @@ Grailbird.data.tweets_2020_09 =
 			"name": "Lee Fallin",
 			"screen_name": "LeeFallin",
 			"location": "United Kingdom",
-			"description": "SFHEA #LearningDeveloper, #EdD student, #MCE & #MIEExpert @UniofHull. I tweet #HigherEd, #EdTech, #Lego, #LDchat & #Hull. All tweets in a personal capacity 🏳️‍🌈",
+			"description": "SFHEA #LearningDeveloper, #EdD student, #MCE & #MIEExpert @UniofHull. I tweet #HigherEd, #EdTech, #Lego, #LDchat & #Hull. All tweets in a personal capacity 🏳️🌈",
 			"url": "https://t.co/UWrodKaQLd",
 			"entities": {
 				"url": {

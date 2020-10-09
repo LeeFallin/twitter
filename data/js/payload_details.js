bac0e2a87c1190063947e9d4cfa94c0ed877010f
LeeFallin <lee@newgecko.net>
2020-10-09 01:21:17 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 10163,
-	"created_at": "Thu Oct 08 00:21:17 +0000 2020",
+	"tweets": 10164,
+	"created_at": "Fri Oct 09 00:21:16 +0000 2020",
 	"lang": "en-gb"
 }
--- data/js/tweets/2020_10.js
+++ data/js/tweets/2020_10.js
@@ -1,6 +1,282 @@
 Grailbird.data.tweets_2020_10 = 
 [
 	{
+		"created_at": "Thu Oct 08 12:40:12 +0000 2020",
+		"id": 1313017498011398100,
+		"id_str": "1314183813409509376",
+		"full_text": "Publication day! Now available FREE from @UCLpress https://t.co/g6eK3Er9uj Pls, download and spread the word. #ableism #AbleismExists #AcademicTwitter #HigherEducation #academia #AbleismInAcademia #Disability #DisabledinSTEM #DisabledRollCall #DisabilityAwareness https://t.co/WMXqcfFo0Z",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			263
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "ableism",
+					"indices": [
+						110,
+						118
+					]
+				},
+				{
+					"text": "AbleismExists",
+					"indices": [
+						119,
+						133
+					]
+				},
+				{
+					"text": "AcademicTwitter",
+					"indices": [
+						134,
+						150
+					]
+				},
+				{
+					"text": "HigherEducation",
+					"indices": [
+						151,
+						167
+					]
+				},
+				{
+					"text": "academia",
+					"indices": [
+						168,
+						177
+					]
+				},
+				{
+					"text": "AbleismInAcademia",
+					"indices": [
+						178,
+						196
+					]
+				},
+				{
+					"text": "Disability",
+					"indices": [
+						197,
+						208
+					]
+				},
+				{
+					"text": "DisabledinSTEM",
+					"indices": [
+						209,
+						224
+					]
+				},
+				{
+					"text": "DisabledRollCall",
+					"indices": [
+						225,
+						242
+					]
+				},
+				{
+					"text": "DisabilityAwareness",
+					"indices": [
+						243,
+						263
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "UCLpress",
+					"name": "UCL Press",
+					"id": 2255340865,
+					"id_str": "2255340865",
+					"indices": [
+						41,
+						50
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/g6eK3Er9uj",
+					"expanded_url": "https://www.uclpress.co.uk/products/123203",
+					"display_url": "uclpress.co.uk/products/123203",
+					"indices": [
+						51,
+						74
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1313017491904434200,
+					"id_str": "1313017491904434178",
+					"indices": [
+						264,
+						287
+					],
+					"media_url": "http://pbs.twimg.com/media/EjjGc8TWkAIEhrL.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EjjGc8TWkAIEhrL.jpg",
+					"url": "https://t.co/WMXqcfFo0Z",
+					"display_url": "pic.twitter.com/WMXqcfFo0Z",
+					"expanded_url": "https://twitter.com/AbleismAcademia/status/1313017498011398144/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 575,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1732,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1015,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1313017491904434200,
+					"id_str": "1313017491904434178",
+					"indices": [
+						264,
+						287
+					],
+					"media_url": "http://pbs.twimg.com/media/EjjGc8TWkAIEhrL.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EjjGc8TWkAIEhrL.jpg",
+					"url": "https://t.co/WMXqcfFo0Z",
+					"display_url": "pic.twitter.com/WMXqcfFo0Z",
+					"expanded_url": "https://twitter.com/AbleismAcademia/status/1313017498011398144/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 575,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1732,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1015,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 963816641317560300,
+			"id_str": "963816641317560322",
+			"name": "Ableism in Academia",
+			"screen_name": "AbleismAcademia",
+			"location": "",
+			"description": "Addressing Ableism in Academia. Launched by @ncjbrown at #AIA2018 Event on 23/03/2018 at UCL. To view click: https://t.co/CYng0TJyh7",
+			"url": "https://t.co/CYng0TJyh7",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/CYng0TJyh7",
+							"expanded_url": "https://youtu.be/6ZScXkO40Pk",
+							"display_url": "youtu.be/6ZScXkO40Pk",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/CYng0TJyh7",
+							"expanded_url": "https://youtu.be/6ZScXkO40Pk",
+							"display_url": "youtu.be/6ZScXkO40Pk",
+							"indices": [
+								109,
+								132
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 1777,
+			"friends_count": 38,
+			"listed_count": 24,
+			"created_at": "Wed Feb 14 16:46:08 +0000 2018",
+			"favourites_count": 777,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 890,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1258866797580877825/9EkTouLP_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1258866797580877825/9EkTouLP_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/963816641317560322/1588972209",
+			"profile_link_color": "19CF86",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 522,
+		"favorite_count": 756,
+		"favorited": true,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Publication day! Now available FREE from @UCLpress https://t.co/g6eK3Er9uj Pls, download and spread the word. #ableism #AbleismExists #AcademicTwitter #HigherEducation #academia #AbleismInAcademia #Disability #DisabledinSTEM #DisabledRollCall #DisabilityAwareness https://t.co/WMXqcfFo0Z"
+	},
+	{
 		"created_at": "Wed Oct 07 13:19:24 +0000 2020",
 		"id": 1313797216776188000,
 		"id_str": "1313831291666739201",

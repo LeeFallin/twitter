d02005fb78e5f2c24ee141553fb74efefa33b098
LeeFallin <lee@newgecko.net>
2020-10-15 01:21:16 +0100

    Added 3 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 10169,
-	"created_at": "Wed Oct 14 00:21:15 +0000 2020",
+	"tweets": 10172,
+	"created_at": "Thu Oct 15 00:21:15 +0000 2020",
 	"lang": "en-gb"
 }
--- data/js/tweets/2020_10.js
+++ data/js/tweets/2020_10.js
@@ -1,6 +1,477 @@
 Grailbird.data.tweets_2020_10 = 
 [
 	{
+		"created_at": "Wed Oct 14 17:10:49 +0000 2020",
+		"id": 1316426243244523500,
+		"id_str": "1316426243244523524",
+		"full_text": "@NomadWarMachine @scholastic_rat @Dr_Paul_Penn @drnsheridan @uob_pgche @hintondm Soooo glad! I don't think my tweet made sense. I sorely miss the #LTHEchat community though :(",
+		"truncated": false,
+		"display_text_range": [
+			81,
+			175
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "LTHEchat",
+					"indices": [
+						146,
+						155
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "NomadWarMachine",
+					"name": "Sarah Honeychurch",
+					"id": 558091832,
+					"id_str": "558091832",
+					"indices": [
+						0,
+						16
+					]
+				},
+				{
+					"screen_name": "scholastic_rat",
+					"name": "Dr Helen Webster 📚🐀",
+					"id": 395277106,
+					"id_str": "395277106",
+					"indices": [
+						17,
+						32
+					]
+				},
+				{
+					"screen_name": "Dr_Paul_Penn",
+					"name": "Dr Paul Penn",
+					"id": 1126081823191326700,
+					"id_str": "1126081823191326720",
+					"indices": [
+						33,
+						46
+					]
+				},
+				{
+					"screen_name": "drnsheridan",
+					"name": "Nathalie *What's next?*",
+					"id": 60389169,
+					"id_str": "60389169",
+					"indices": [
+						47,
+						59
+					]
+				},
+				{
+					"screen_name": "uob_pgche",
+					"name": "University of Birmingham PGCHE",
+					"id": 1085484184318214100,
+					"id_str": "1085484184318214145",
+					"indices": [
+						60,
+						70
+					]
+				},
+				{
+					"screen_name": "hintondm",
+					"name": "Danielle M Hinton 👩🏻‍🏫👩🏻‍💻🌍",
+					"id": 36370231,
+					"id_str": "36370231",
+					"indices": [
+						71,
+						80
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": 1316424084859883500,
+		"in_reply_to_status_id_str": "1316424084859883524",
+		"in_reply_to_user_id": 558091832,
+		"in_reply_to_user_id_str": "558091832",
+		"in_reply_to_screen_name": "NomadWarMachine",
+		"user": {
+			"id": 55448837,
+			"id_str": "55448837",
+			"name": "Lee Fallin",
+			"screen_name": "LeeFallin",
+			"location": "United Kingdom",
+			"description": "SFHEA #LearningDeveloper, #EdD student, #MCE & #MIEExpert @UniofHull. I tweet #HigherEd, #EdTech, #Lego, #LDchat & #Hull. All tweets in a personal capacity 🏳️‍🌈",
+			"url": "https://t.co/UWrodKaQLd",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/UWrodKaQLd",
+							"expanded_url": "http://www.leefallin.co.uk",
+							"display_url": "leefallin.co.uk",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 1379,
+			"friends_count": 1212,
+			"listed_count": 62,
+			"created_at": "Fri Jul 10 02:53:40 +0000 2009",
+			"favourites_count": 7042,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 10282,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/753894560108011520/7h68mawt_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/753894560108011520/7h68mawt_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/55448837/1555627842",
+			"profile_link_color": "1B95E0",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 4,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "en",
+		"text": "@NomadWarMachine @scholastic_rat @Dr_Paul_Penn @drnsheridan @uob_pgche @hintondm Soooo glad! I don't think my tweet made sense. I sorely miss the #LTHEchat community though :("
+	},
+	{
+		"created_at": "Wed Oct 14 17:00:04 +0000 2020",
+		"id": 1316423538912485400,
+		"id_str": "1316423538912485376",
+		"full_text": "@NomadWarMachine @scholastic_rat @Dr_Paul_Penn @drnsheridan @uob_pgche @hintondm I need to get back to it. It just feels more of a thing now. Once I week at home I snuggled up with my phone and tweeted. Now I work from home, it seems more exhausting to spend more time intellectually engaging now they’re the same space.",
+		"truncated": false,
+		"display_text_range": [
+			81,
+			320
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "NomadWarMachine",
+					"name": "Sarah Honeychurch",
+					"id": 558091832,
+					"id_str": "558091832",
+					"indices": [
+						0,
+						16
+					]
+				},
+				{
+					"screen_name": "scholastic_rat",
+					"name": "Dr Helen Webster 📚🐀",
+					"id": 395277106,
+					"id_str": "395277106",
+					"indices": [
+						17,
+						32
+					]
+				},
+				{
+					"screen_name": "Dr_Paul_Penn",
+					"name": "Dr Paul Penn",
+					"id": 1126081823191326700,
+					"id_str": "1126081823191326720",
+					"indices": [
+						33,
+						46
+					]
+				},
+				{
+					"screen_name": "drnsheridan",
+					"name": "Nathalie *What's next?*",
+					"id": 60389169,
+					"id_str": "60389169",
+					"indices": [
+						47,
+						59
+					]
+				},
+				{
+					"screen_name": "uob_pgche",
+					"name": "University of Birmingham PGCHE",
+					"id": 1085484184318214100,
+					"id_str": "1085484184318214145",
+					"indices": [
+						60,
+						70
+					]
+				},
+				{
+					"screen_name": "hintondm",
+					"name": "Danielle M Hinton 👩🏻‍🏫👩🏻‍💻🌍",
+					"id": 36370231,
+					"id_str": "36370231",
+					"indices": [
+						71,
+						80
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": 1316367480386920400,
+		"in_reply_to_status_id_str": "1316367480386920448",
+		"in_reply_to_user_id": 558091832,
+		"in_reply_to_user_id_str": "558091832",
+		"in_reply_to_screen_name": "NomadWarMachine",
+		"user": {
+			"id": 55448837,
+			"id_str": "55448837",
+			"name": "Lee Fallin",
+			"screen_name": "LeeFallin",
+			"location": "United Kingdom",
+			"description": "SFHEA #LearningDeveloper, #EdD student, #MCE & #MIEExpert @UniofHull. I tweet #HigherEd, #EdTech, #Lego, #LDchat & #Hull. All tweets in a personal capacity 🏳️‍🌈",
+			"url": "https://t.co/UWrodKaQLd",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/UWrodKaQLd",
+							"expanded_url": "http://www.leefallin.co.uk",
+							"display_url": "leefallin.co.uk",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 1379,
+			"friends_count": 1212,
+			"listed_count": 62,
+			"created_at": "Fri Jul 10 02:53:40 +0000 2009",
+			"favourites_count": 7042,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 10282,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/753894560108011520/7h68mawt_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/753894560108011520/7h68mawt_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/55448837/1555627842",
+			"profile_link_color": "1B95E0",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "en",
+		"text": "@NomadWarMachine @scholastic_rat @Dr_Paul_Penn @drnsheridan @uob_pgche @hintondm I need to get back to it. It just feels more of a thing now. Once I week at home I snuggled up with my phone and tweeted. Now I work from home, it seems more exhausting to spend more time intellectually engaging now they’re the same space."
+	},
+	{
+		"created_at": "Wed Oct 14 10:17:55 +0000 2020",
+		"id": 1316320396715733000,
+		"id_str": "1316322333972328448",
+		"full_text": "21% of HE students do not discuss digital skills... only 34% get opportunity to assess their digital skills eg career planning...#jiscexperts20 @N4LTH @N4LTH @aldinhe_LH #loveLD @sarahknight",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			190
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "jiscexperts20",
+					"indices": [
+						129,
+						143
+					]
+				},
+				{
+					"text": "loveLD",
+					"indices": [
+						170,
+						177
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "N4LTH",
+					"name": "N4LTH Nursing for Long Term Health- BU University",
+					"id": 1100355309867094000,
+					"id_str": "1100355309867094016",
+					"indices": [
+						144,
+						150
+					]
+				},
+				{
+					"screen_name": "N4LTH",
+					"name": "N4LTH Nursing for Long Term Health- BU University",
+					"id": 1100355309867094000,
+					"id_str": "1100355309867094016",
+					"indices": [
+						151,
+						157
+					]
+				},
+				{
+					"screen_name": "aldinhe_LH",
+					"name": "ALDinHE",
+					"id": 119064069,
+					"id_str": "119064069",
+					"indices": [
+						158,
+						169
+					]
+				},
+				{
+					"screen_name": "sarahknight",
+					"name": "sarahknight #FE #HE",
+					"id": 17208485,
+					"id_str": "17208485",
+					"indices": [
+						178,
+						190
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 77721089,
+			"id_str": "77721089",
+			"name": "Dr Debbie Holley",
+			"screen_name": "DebbieHolley1",
+			"location": "UK",
+			"description": "Professor of Learning Innovation, FHSS, Bournemouth University. Research Interests: students/technology, committed to OER Opinions my own.",
+			"url": "https://t.co/7t3BrWGpwd",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/7t3BrWGpwd",
+							"expanded_url": "http://drdebbieholley.com",
+							"display_url": "drdebbieholley.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 1963,
+			"friends_count": 1052,
+			"listed_count": 246,
+			"created_at": "Sun Sep 27 12:03:13 +0000 2009",
+			"favourites_count": 10834,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 26890,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1114516058977853440/WchNc7yZ_normal.png",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1114516058977853440/WchNc7yZ_normal.png",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/77721089/1554556397",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1,
+		"favorite_count": 3,
+		"favorited": false,
+		"retweeted": true,
+		"lang": "en",
+		"text": "21% of HE students do not discuss digital skills... only 34% get opportunity to assess their digital skills eg career planning...#jiscexperts20 @N4LTH @N4LTH @aldinhe_LH #loveLD @sarahknight"
+	},
+	{
 		"created_at": "Tue Oct 13 17:33:20 +0000 2020",
 		"id": 1316069522101633000,
 		"id_str": "1316069522101633026",
@@ -59,7 +530,7 @@ Grailbird.data.tweets_2020_10 =
 			"name": "Lee Fallin",
 			"screen_name": "LeeFallin",
 			"location": "United Kingdom",
-			"description": "SFHEA #LearningDeveloper, #EdD student, #MCE & #MIEExpert @UniofHull. I tweet #HigherEd, #EdTech, #Lego, #LDchat & #Hull. All tweets in a personal capacity 🏳️‍🌈",
+			"description": "SFHEA #LearningDeveloper, #EdD student, #MCE & #MIEExpert @UniofHull. I tweet #HigherEd, #EdTech, #Lego, #LDchat & #Hull. All tweets in a personal capacity 🏳️🌈",
 			"url": "https://t.co/UWrodKaQLd",
 			"entities": {
 				"url": {

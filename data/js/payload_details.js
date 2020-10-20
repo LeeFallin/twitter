4ed978b7bb27998c3edc18176aa88d0a82a0bf67
LeeFallin <lee@newgecko.net>
2020-10-20 01:21:17 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 10173,
-	"created_at": "Fri Oct 16 00:21:17 +0000 2020",
+	"tweets": 10174,
+	"created_at": "Tue Oct 20 00:21:16 +0000 2020",
 	"lang": "en-gb"
 }
--- data/js/tweets/2020_10.js
+++ data/js/tweets/2020_10.js
@@ -1,6 +1,120 @@
 Grailbird.data.tweets_2020_10 = 
 [
 	{
+		"created_at": "Mon Oct 19 10:31:49 +0000 2020",
+		"id": 1318137328989360000,
+		"id_str": "1318137770708226048",
+		"full_text": "\"Tourism provides opportunities for [people] to slow down and enjoy ordinary activities ... so that they might evade the cult of speed\" \n\nDuo Yin, Qingyan Huang &amp; Xiaobo Su's #GeographicalJournal paper now in Early View\n\nhttps://t.co/52mm9dIdGR",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			248
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "GeographicalJournal",
+					"indices": [
+						179,
+						199
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/52mm9dIdGR",
+					"expanded_url": "http://orlo.uk/cfS4U",
+					"display_url": "orlo.uk/cfS4U",
+					"indices": [
+						225,
+						248
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://orlo.tech\" rel=\"nofollow\">Orlo</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 3030504268,
+			"id_str": "3030504268",
+			"name": "RGS-IBG Higher Ed",
+			"screen_name": "RGS_IBGhe",
+			"location": "Exhibition Road, London",
+			"description": "The latest in #geography higher education for undergraduates, postgraduates and academics from @RGS_IBG. Retweets ≠ endorsements.",
+			"url": "http://t.co/okR3UiPepC",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "http://t.co/okR3UiPepC",
+							"expanded_url": "http://www.rgs.org/rhed",
+							"display_url": "rgs.org/rhed",
+							"indices": [
+								0,
+								22
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 4067,
+			"friends_count": 262,
+			"listed_count": 85,
+			"created_at": "Wed Feb 11 14:53:11 +0000 2015",
+			"favourites_count": 3547,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 9949,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/565524283045056512/UWEdEZQe_normal.png",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/565524283045056512/UWEdEZQe_normal.png",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3030504268/1423667968",
+			"profile_link_color": "740160",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 2,
+		"favorite_count": 1,
+		"favorited": true,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "\"Tourism provides opportunities for [people] to slow down and enjoy ordinary activities ... so that they might evade the cult of speed\" \n\nDuo Yin, Qingyan Huang &amp; Xiaobo Su's #GeographicalJournal paper now in Early View\n\nhttps://t.co/52mm9dIdGR"
+	},
+	{
 		"created_at": "Thu Oct 15 07:13:41 +0000 2020",
 		"id": 1316338450619084800,
 		"id_str": "1316638361218674689",

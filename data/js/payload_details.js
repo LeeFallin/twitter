184f3badfa3583e15d07583e91bd3932ec1277dc
LeeFallin <lee@newgecko.net>
2020-10-27 00:21:17 +0000

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 10174,
-	"created_at": "Tue Oct 20 00:21:16 +0000 2020",
+	"tweets": 10175,
+	"created_at": "Tue Oct 27 00:21:16 +0000 2020",
 	"lang": "en-gb"
 }

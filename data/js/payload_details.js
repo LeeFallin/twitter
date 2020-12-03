7bc20a7ed848d6bc48849be6a9696fb2157ba745
LeeFallin <lee@newgecko.net>
2020-12-03 00:21:17 +0000

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 10237,
-	"created_at": "Tue Dec 01 00:22:55 +0000 2020",
+	"tweets": 10238,
+	"created_at": "Thu Dec 03 00:21:16 +0000 2020",
 	"lang": "en-gb"
 }
---
+++ data/js/tweets/2020_12.js
@@ -0,0 +1,236 @@
+Grailbird.data.tweets_2020_12 = 
+[
+	{
+		"created_at": "Wed Dec 02 08:14:51 +0000 2020",
+		"id": 1334038340484690000,
+		"id_str": "1334048368029528065",
+		"full_text": "This video introduces the topic of text size in video games, shows various demos of how text size can affect accessibility, and provides guidelines for game developers. \n\nCheck it out: https://t.co/I3Lu4G7L6K #A11y #Gaming https://t.co/IAh9p6wiaF",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			222
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "A11y",
+					"indices": [
+						209,
+						214
+					]
+				},
+				{
+					"text": "Gaming",
+					"indices": [
+						215,
+						222
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/I3Lu4G7L6K",
+					"expanded_url": "https://msft.it/6015TUPv5",
+					"display_url": "msft.it/6015TUPv5",
+					"indices": [
+						185,
+						208
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1294038523918483500,
+					"id_str": "1294038523918483456",
+					"indices": [
+						223,
+						246
+					],
+					"media_url": "http://pbs.twimg.com/media/EoNSsgzUwAAhp8B.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EoNSsgzUwAAhp8B.jpg",
+					"url": "https://t.co/IAh9p6wiaF",
+					"display_url": "pic.twitter.com/IAh9p6wiaF",
+					"expanded_url": "https://twitter.com/MSFTEnable/status/1334038340484689920/video/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 640,
+							"h": 360,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 640,
+							"h": 360,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 640,
+							"h": 360,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1294038523918483500,
+					"id_str": "1294038523918483456",
+					"indices": [
+						223,
+						246
+					],
+					"media_url": "http://pbs.twimg.com/media/EoNSsgzUwAAhp8B.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EoNSsgzUwAAhp8B.jpg",
+					"url": "https://t.co/IAh9p6wiaF",
+					"display_url": "pic.twitter.com/IAh9p6wiaF",
+					"expanded_url": "https://twitter.com/MSFTEnable/status/1334038340484689920/video/1",
+					"type": "video",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 640,
+							"h": 360,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 640,
+							"h": 360,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 640,
+							"h": 360,
+							"resize": "fit"
+						}
+					},
+					"video_info": {
+						"aspect_ratio": [
+							16,
+							9
+						],
+						"duration_millis": 98400,
+						"variants": [
+							{
+								"bitrate": 832000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/amplify_video/1294038523918483456/vid/640x360/ip1B_kRinct6cnK1.mp4?tag=13"
+							},
+							{
+								"bitrate": 288000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/amplify_video/1294038523918483456/vid/480x270/3JifX7IE6zn4NJZo.mp4?tag=13"
+							},
+							{
+								"content_type": "application/x-mpegURL",
+								"url": "https://video.twimg.com/amplify_video/1294038523918483456/pl/r-kq6ANHVI-IIPDC.m3u8?tag=13"
+							}
+						]
+					},
+					"additional_media_info": {
+						"title": "Text Size in Video Games: How Big Should It Be?",
+						"description": "This video introduces the topic of text size in video games, shows various demos of how text size can affect accessibility, and provides guidelines for game developers.",
+						"call_to_actions": {
+							"visit_site": {
+								"url": "https://www.youtube.com/watch?reload=9&v=qjFvQKsvyXw&feature=youtu.be"
+							}
+						},
+						"embeddable": true,
+						"monetizable": false
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://ads.twitter.com\" rel=\"nofollow\">Twitter Ads</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 458619342,
+			"id_str": "458619342",
+			"name": "MSFT Accessibility",
+			"screen_name": "MSFTEnable",
+			"location": "Redmond, WA",
+			"description": "Empowering through accessibility, for every person and every organization on the planet to achieve more.",
+			"url": "https://t.co/nyzY7wGZ7P",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/nyzY7wGZ7P",
+							"expanded_url": "http://www.microsoft.com/accessibility",
+							"display_url": "microsoft.com/accessibility",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 35101,
+			"friends_count": 10296,
+			"listed_count": 841,
+			"created_at": "Sun Jan 08 19:26:40 +0000 2012",
+			"favourites_count": 60313,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 15482,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1268210202001551360/JEBBFQr1_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1268210202001551360/JEBBFQr1_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/458619342/1595546255",
+			"profile_link_color": "0084B4",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 20,
+		"favorite_count": 28,
+		"favorited": true,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "This video introduces the topic of text size in video games, shows various demos of how text size can affect accessibility, and provides guidelines for game developers. \n\nCheck it out: https://t.co/I3Lu4G7L6K #A11y #Gaming https://t.co/IAh9p6wiaF"
+	}
+]

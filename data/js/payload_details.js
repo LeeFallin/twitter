13b257d15706391bdd8d3d3bba6ad93a9761d211
LeeFallin <lee@newgecko.net>
2020-12-05 00:21:17 +0000

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 10239,
-	"created_at": "Fri Dec 04 00:21:26 +0000 2020",
+	"tweets": 10240,
+	"created_at": "Sat Dec 05 00:21:15 +0000 2020",
 	"lang": "en-gb"
 }
--- data/js/tweets/2020_12.js
+++ data/js/tweets/2020_12.js
@@ -1,6 +1,346 @@
 Grailbird.data.tweets_2020_12 = 
 [
 	{
+		"created_at": "Fri Dec 04 13:17:45 +0000 2020",
+		"id": 1334849374455787500,
+		"id_str": "1334849374455787522",
+		"full_text": "Really enjoying this session- especially the inclusion of speed and timeliness as an aspect of quality research. Fast doesn’t mean poor. https://t.co/8FiJvVMTzL",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			136
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/8FiJvVMTzL",
+					"expanded_url": "https://twitter.com/nvivobyqsr/status/1333448346804084736",
+					"display_url": "twitter.com/nvivobyqsr/sta…",
+					"indices": [
+						137,
+						160
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 55448837,
+			"id_str": "55448837",
+			"name": "Dr Lee Fallin",
+			"screen_name": "LeeFallin",
+			"location": "United Kingdom",
+			"description": "SFHEA EdD FRGS 🏳️‍🌈 #LearningDeveloper, #MCE & #MIEExpert @UniofHull. I tweet #HigherEd, #EdTech, #Lego, #loveLD & #Hull. All tweets in a personal capacity",
+			"url": "https://t.co/UWrodKaQLd",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/UWrodKaQLd",
+							"expanded_url": "http://www.leefallin.co.uk",
+							"display_url": "leefallin.co.uk",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 1389,
+			"friends_count": 1227,
+			"listed_count": 61,
+			"created_at": "Fri Jul 10 02:53:40 +0000 2009",
+			"favourites_count": 7180,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 10347,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/753894560108011520/7h68mawt_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/753894560108011520/7h68mawt_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/55448837/1555627842",
+			"profile_link_color": "1B95E0",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": {
+			"id": "7bc0da87641dd821",
+			"url": "https://api.twitter.com/1.1/geo/id/7bc0da87641dd821.json",
+			"place_type": "city",
+			"name": "Hull",
+			"full_name": "Hull, England",
+			"country_code": "GB",
+			"country": "United Kingdom",
+			"contained_within": [],
+			"bounding_box": {
+				"type": "Polygon",
+				"coordinates": [
+					[
+						[
+							-0.422572,
+							53.719616
+						],
+						[
+							-0.2453134,
+							53.719616
+						],
+						[
+							-0.2453134,
+							53.812018
+						],
+						[
+							-0.422572,
+							53.812018
+						]
+					]
+				]
+			},
+			"attributes": {}
+		},
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1333448346804084700,
+		"quoted_status_id_str": "1333448346804084736",
+		"quoted_status_permalink": {
+			"url": "https://t.co/8FiJvVMTzL",
+			"expanded": "https://twitter.com/nvivobyqsr/status/1333448346804084736",
+			"display": "twitter.com/nvivobyqsr/sta…"
+		},
+		"quoted_status": {
+			"created_at": "Mon Nov 30 16:30:34 +0000 2020",
+			"id": 1333448346804084700,
+			"id_str": "1333448346804084736",
+			"full_text": "Reminder! Register for the fourth NVivo &amp; SAGE Publishing (@SAGE_Methods) Fall Series webinar on December 4, Cecilia Vindrola-Padros, University of College London, An Introduction to Rapid Qualitative Research.\nhttps://t.co/y6Vya4NmVN https://t.co/MyGRnySxy9",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				238
+			],
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [
+					{
+						"screen_name": "SAGE_Methods",
+						"name": "SAGE Methods",
+						"id": 17129326,
+						"id_str": "17129326",
+						"indices": [
+							63,
+							76
+						]
+					}
+				],
+				"urls": [
+					{
+						"url": "https://t.co/y6Vya4NmVN",
+						"expanded_url": "https://bit.ly/35AOc0n",
+						"display_url": "bit.ly/35AOc0n",
+						"indices": [
+							215,
+							238
+						]
+					}
+				],
+				"media": [
+					{
+						"id": 1333448345587736600,
+						"id_str": "1333448345587736578",
+						"indices": [
+							239,
+							262
+						],
+						"media_url": "http://pbs.twimg.com/media/EoFcNJUW4AIzE6R.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/EoFcNJUW4AIzE6R.jpg",
+						"url": "https://t.co/MyGRnySxy9",
+						"display_url": "pic.twitter.com/MyGRnySxy9",
+						"expanded_url": "https://twitter.com/NVivobyQSR/status/1333448346804084736/photo/1",
+						"type": "photo",
+						"sizes": {
+							"large": {
+								"w": 300,
+								"h": 131,
+								"resize": "fit"
+							},
+							"thumb": {
+								"w": 131,
+								"h": 131,
+								"resize": "crop"
+							},
+							"small": {
+								"w": 300,
+								"h": 131,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 300,
+								"h": 131,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 1333448345587736600,
+						"id_str": "1333448345587736578",
+						"indices": [
+							239,
+							262
+						],
+						"media_url": "http://pbs.twimg.com/media/EoFcNJUW4AIzE6R.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/EoFcNJUW4AIzE6R.jpg",
+						"url": "https://t.co/MyGRnySxy9",
+						"display_url": "pic.twitter.com/MyGRnySxy9",
+						"expanded_url": "https://twitter.com/NVivobyQSR/status/1333448346804084736/photo/1",
+						"type": "photo",
+						"sizes": {
+							"large": {
+								"w": 300,
+								"h": 131,
+								"resize": "fit"
+							},
+							"thumb": {
+								"w": 131,
+								"h": 131,
+								"resize": "crop"
+							},
+							"small": {
+								"w": 300,
+								"h": 131,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 300,
+								"h": 131,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"source": "<a href=\"https://www.hootsuite.com\" rel=\"nofollow\">Hootsuite Inc.</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 221262027,
+				"id_str": "221262027",
+				"name": "NVivo by QSR",
+				"screen_name": "NVivobyQSR",
+				"location": "Worldwide",
+				"description": "Your official Twitter source for all things #NVivo! Tweeting 9am-5pm EST M-F. Ping @NVivoSup for software support!",
+				"url": "https://t.co/YorSBXmXzg",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/YorSBXmXzg",
+								"expanded_url": "https://www.qsrinternational.com/nvivo/home",
+								"display_url": "qsrinternational.com/nvivo/home",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 5509,
+				"friends_count": 1456,
+				"listed_count": 119,
+				"created_at": "Tue Nov 30 05:08:04 +0000 2010",
+				"favourites_count": 1859,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": true,
+				"verified": false,
+				"statuses_count": 5719,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "0D4E90",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/975990782237667328/MLdnuffA_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/975990782237667328/MLdnuffA_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/221262027/1584565568",
+				"profile_link_color": "189AD6",
+				"profile_sidebar_border_color": "FFFFFF",
+				"profile_sidebar_fill_color": "E2F1F5",
+				"profile_text_color": "017CBF",
+				"profile_use_background_image": false,
+				"has_extended_profile": false,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": true,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 2,
+			"favorite_count": 5,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Really enjoying this session- especially the inclusion of speed and timeliness as an aspect of quality research. Fast doesn’t mean poor. https://t.co/8FiJvVMTzL"
+	},
+	{
 		"created_at": "Thu Dec 03 15:53:13 +0000 2020",
 		"id": 1333803044106956800,
 		"id_str": "1334526108620951554",

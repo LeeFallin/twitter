4d86201029dcb36133bf335277852a712316fc84
LeeFallin <lee@newgecko.net>
2021-04-04 01:21:15 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 10287,
-	"created_at": "Wed Mar 31 00:21:15 +0000 2021",
+	"tweets": 10288,
+	"created_at": "Sun Apr 04 00:21:15 +0000 2021",
 	"lang": "en-gb"
 }

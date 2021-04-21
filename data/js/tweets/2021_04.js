27a9027ab0ac75435a64922d6084057b0a37b210
LeeFallin <lee@newgecko.net>
2021-04-21 01:21:18 +0100

    Added 1 tweets

--- data/js/tweets/2021_04.js
+++ data/js/tweets/2021_04.js
@@ -1,6 +1,228 @@
 Grailbird.data.tweets_2021_04 = 
 [
 	{
+		"created_at": "Tue Apr 20 15:05:27 +0000 2021",
+		"id": 1384477209155367000,
+		"id_str": "1384523615371149315",
+		"full_text": "RT + Follow @XboxUK and you can be in with the chance of winning this mega LEGO bundle ⬇️\n\n🎮 11 LEGO video games on Xbox featuring new, iconic cover art\n📺 4 LEGO movies to watch\n🤡 Joker Trike Chase LEGO brick set\n🔮 Harry Potter Potions Class brick set https://t.co/jd8xQE8GQ9",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			251
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "xboxuk",
+					"name": "Xbox UK",
+					"id": 196618084,
+					"id_str": "196618084",
+					"indices": [
+						12,
+						19
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 1384464923464327200,
+					"id_str": "1384464923464327170",
+					"indices": [
+						252,
+						275
+					],
+					"media_url": "http://pbs.twimg.com/media/Ezabgz8UYAIMgp3.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Ezabgz8UYAIMgp3.jpg",
+					"url": "https://t.co/jd8xQE8GQ9",
+					"display_url": "pic.twitter.com/jd8xQE8GQ9",
+					"expanded_url": "https://twitter.com/xboxuk/status/1384477209155366913/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 2048,
+							"h": 1367,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 801,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 454,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1384464923464327200,
+					"id_str": "1384464923464327170",
+					"indices": [
+						252,
+						275
+					],
+					"media_url": "http://pbs.twimg.com/media/Ezabgz8UYAIMgp3.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Ezabgz8UYAIMgp3.jpg",
+					"url": "https://t.co/jd8xQE8GQ9",
+					"display_url": "pic.twitter.com/jd8xQE8GQ9",
+					"expanded_url": "https://twitter.com/xboxuk/status/1384477209155366913/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 2048,
+							"h": 1367,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 801,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 454,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1384464961175396400,
+					"id_str": "1384464961175396352",
+					"indices": [
+						252,
+						275
+					],
+					"media_url": "http://pbs.twimg.com/media/EzabjAbVoAAxLlH.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EzabjAbVoAAxLlH.jpg",
+					"url": "https://t.co/jd8xQE8GQ9",
+					"display_url": "pic.twitter.com/jd8xQE8GQ9",
+					"expanded_url": "https://twitter.com/xboxuk/status/1384477209155366913/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1652,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 549,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 968,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 196618084,
+			"id_str": "196618084",
+			"name": "Xbox UK",
+			"screen_name": "xboxuk",
+			"location": "UK",
+			"description": "The official Xbox UK Twitter account 💚🇬🇧\nTech troubles? Please check in with @XboxSupport",
+			"url": "https://t.co/h0UWjm6zBV",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/h0UWjm6zBV",
+							"expanded_url": "http://www.xbox.com",
+							"display_url": "xbox.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 878464,
+			"friends_count": 1670,
+			"listed_count": 0,
+			"created_at": "Wed Sep 29 14:45:55 +0000 2010",
+			"favourites_count": 7390,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 42625,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1372577722598309889/cIQ81HgK_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1372577722598309889/cIQ81HgK_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/196618084/1616083007",
+			"profile_link_color": "107C10",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "21B830",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 3534,
+		"favorite_count": 1614,
+		"favorited": true,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "RT + Follow @XboxUK and you can be in with the chance of winning this mega LEGO bundle ⬇️\n\n🎮 11 LEGO video games on Xbox featuring new, iconic cover art\n📺 4 LEGO movies to watch\n🤡 Joker Trike Chase LEGO brick set\n🔮 Harry Potter Potions Class brick set https://t.co/jd8xQE8GQ9"
+	},
+	{
 		"created_at": "Sun Apr 18 06:21:14 +0000 2021",
 		"id": 1383431187528437800,
 		"id_str": "1383666914761723904",

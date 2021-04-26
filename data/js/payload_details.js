b09885596a09d9a03373990fdcd8d8bb823757ce
LeeFallin <lee@newgecko.net>
2021-04-26 01:21:17 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 10391,
-	"created_at": "Wed Apr 21 00:21:17 +0000 2021",
+	"tweets": 10392,
+	"created_at": "Mon Apr 26 00:21:16 +0000 2021",
 	"lang": "en-gb"
 }
--- data/js/tweets/2021_04.js
+++ data/js/tweets/2021_04.js
@@ -1,6 +1,168 @@
 Grailbird.data.tweets_2021_04 = 
 [
 	{
+		"created_at": "Sun Apr 25 21:38:58 +0000 2021",
+		"id": 1386232318243840000,
+		"id_str": "1386434585161084930",
+		"full_text": "https://t.co/VqPh9nrKRE",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			0
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1386232314938671000,
+					"id_str": "1386232314938671108",
+					"indices": [
+						0,
+						23
+					],
+					"media_url": "http://pbs.twimg.com/media/Ezzi8iQWQAQKK9n.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Ezzi8iQWQAQKK9n.jpg",
+					"url": "https://t.co/VqPh9nrKRE",
+					"display_url": "pic.twitter.com/VqPh9nrKRE",
+					"expanded_url": "https://twitter.com/PedagogyPanda/status/1386232318243840001/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 750,
+							"h": 500,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 453,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 750,
+							"h": 500,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1386232314938671000,
+					"id_str": "1386232314938671108",
+					"indices": [
+						0,
+						23
+					],
+					"media_url": "http://pbs.twimg.com/media/Ezzi8iQWQAQKK9n.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Ezzi8iQWQAQKK9n.jpg",
+					"url": "https://t.co/VqPh9nrKRE",
+					"display_url": "pic.twitter.com/VqPh9nrKRE",
+					"expanded_url": "https://twitter.com/PedagogyPanda/status/1386232318243840001/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 750,
+							"h": 500,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 453,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 750,
+							"h": 500,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/#!/download/ipad\" rel=\"nofollow\">Twitter for iPad</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1101132192087003100,
+			"id_str": "1101132192087003137",
+			"name": "The Pedagogical Panda",
+			"screen_name": "PedagogyPanda",
+			"location": "In the forests",
+			"description": "What about the pedagogy?",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 583,
+			"friends_count": 98,
+			"listed_count": 4,
+			"created_at": "Thu Feb 28 14:49:07 +0000 2019",
+			"favourites_count": 99,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 375,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1101197335592427523/-MMXMLQD_normal.png",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1101197335592427523/-MMXMLQD_normal.png",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1101132192087003137/1551367280",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 14,
+		"favorite_count": 49,
+		"favorited": true,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "und",
+		"text": "https://t.co/VqPh9nrKRE"
+	},
+	{
 		"created_at": "Tue Apr 20 15:05:27 +0000 2021",
 		"id": 1384477209155367000,
 		"id_str": "1384523615371149315",

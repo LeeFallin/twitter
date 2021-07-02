3bbc6302a099a57cf535f84691f2c5552c64c109
LeeFallin <lee@newgecko.net>
2021-07-02 01:21:16 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 10444,
-	"created_at": "Thu Jul 01 00:21:15 +0000 2021",
+	"tweets": 10445,
+	"created_at": "Fri Jul 02 00:21:15 +0000 2021",
 	"lang": "en-gb"
 }
---
+++ data/js/tweets/2021_07.js
@@ -0,0 +1,323 @@
+Grailbird.data.tweets_2021_07 = 
+[
+	{
+		"created_at": "Thu Jul 01 14:38:04 +0000 2021",
+		"id": 1410608650863841300,
+		"id_str": "1410608650863841297",
+		"full_text": "Whoooop! https://t.co/jtwXxFVzNp",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			8
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/jtwXxFVzNp",
+					"expanded_url": "https://twitter.com/mike_ewen/status/1410608358676078628",
+					"display_url": "twitter.com/mike_ewen/stat…",
+					"indices": [
+						9,
+						32
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 55448837,
+			"id_str": "55448837",
+			"name": "Dr Lee Fallin",
+			"screen_name": "LeeFallin",
+			"location": "United Kingdom",
+			"description": "SFHEA EdD FRGS 🏳️‍🌈 #LearningDeveloper, #MCE & #MIEExpert @UniofHull. I tweet #HigherEd, #EdTech, #Lego, #loveLD & #Hull. All tweets in a personal capacity",
+			"url": "https://t.co/UWrodKaQLd",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/UWrodKaQLd",
+							"expanded_url": "http://www.leefallin.co.uk",
+							"display_url": "leefallin.co.uk",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 1427,
+			"friends_count": 1250,
+			"listed_count": 61,
+			"created_at": "Fri Jul 10 02:53:40 +0000 2009",
+			"favourites_count": 7518,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 10580,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/753894560108011520/7h68mawt_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/753894560108011520/7h68mawt_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/55448837/1555627842",
+			"profile_link_color": "1B95E0",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1410608358676078600,
+		"quoted_status_id_str": "1410608358676078628",
+		"quoted_status_permalink": {
+			"url": "https://t.co/jtwXxFVzNp",
+			"expanded": "https://twitter.com/mike_ewen/status/1410608358676078628",
+			"display": "twitter.com/mike_ewen/stat…"
+		},
+		"quoted_status": {
+			"created_at": "Thu Jul 01 14:36:55 +0000 2021",
+			"id": 1410608358676078600,
+			"id_str": "1410608358676078628",
+			"full_text": "Delighted to have been awarded Senior   Fellow of the HEA. Thanks to everyone at @UoHAcademy and beyond for supporting me in developing the application and allowing me to take credit for all your hard work 😀 #sfhea https://t.co/whGuiyAC10",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				214
+			],
+			"entities": {
+				"hashtags": [
+					{
+						"text": "sfhea",
+						"indices": [
+							208,
+							214
+						]
+					}
+				],
+				"symbols": [],
+				"user_mentions": [
+					{
+						"screen_name": "UoHAcademy",
+						"name": "Teaching Excellence Academy",
+						"id": 914481006,
+						"id_str": "914481006",
+						"indices": [
+							81,
+							92
+						]
+					}
+				],
+				"urls": [],
+				"media": [
+					{
+						"id": 1410608320201695200,
+						"id_str": "1410608320201695242",
+						"indices": [
+							215,
+							238
+						],
+						"media_url": "http://pbs.twimg.com/tweet_video_thumb/E5N8yyLVIAojtkm.jpg",
+						"media_url_https": "https://pbs.twimg.com/tweet_video_thumb/E5N8yyLVIAojtkm.jpg",
+						"url": "https://t.co/whGuiyAC10",
+						"display_url": "pic.twitter.com/whGuiyAC10",
+						"expanded_url": "https://twitter.com/mike_ewen/status/1410608358676078628/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"medium": {
+								"w": 468,
+								"h": 498,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 468,
+								"h": 498,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 468,
+								"h": 498,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 1410608320201695200,
+						"id_str": "1410608320201695242",
+						"indices": [
+							215,
+							238
+						],
+						"media_url": "http://pbs.twimg.com/tweet_video_thumb/E5N8yyLVIAojtkm.jpg",
+						"media_url_https": "https://pbs.twimg.com/tweet_video_thumb/E5N8yyLVIAojtkm.jpg",
+						"url": "https://t.co/whGuiyAC10",
+						"display_url": "pic.twitter.com/whGuiyAC10",
+						"expanded_url": "https://twitter.com/mike_ewen/status/1410608358676078628/photo/1",
+						"type": "animated_gif",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"medium": {
+								"w": 468,
+								"h": 498,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 468,
+								"h": 498,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 468,
+								"h": 498,
+								"resize": "fit"
+							}
+						},
+						"video_info": {
+							"aspect_ratio": [
+								78,
+								83
+							],
+							"variants": [
+								{
+									"bitrate": 0,
+									"content_type": "video/mp4",
+									"url": "https://video.twimg.com/tweet_video/E5N8yyLVIAojtkm.mp4"
+								}
+							]
+						}
+					}
+				]
+			},
+			"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 20668436,
+				"id_str": "20668436",
+				"name": "Mike Ewen",
+				"screen_name": "mike_ewen",
+				"location": "Hull",
+				"description": "Teaching Enhancement Adviser at @UniofHull. Interested in all things social media and digital innovation. Views are my own.",
+				"url": "https://t.co/4y5aVfXdEH",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/4y5aVfXdEH",
+								"expanded_url": "https://mikeewen101.wordpress.com/",
+								"display_url": "mikeewen101.wordpress.com",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 945,
+				"friends_count": 1250,
+				"listed_count": 39,
+				"created_at": "Thu Feb 12 11:12:45 +0000 2009",
+				"favourites_count": 4485,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": true,
+				"verified": false,
+				"statuses_count": 6700,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "C0DEED",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/772515705426178048/uavHGWXV_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/772515705426178048/uavHGWXV_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/20668436/1353692258",
+				"profile_link_color": "1DA1F2",
+				"profile_sidebar_border_color": "C0DEED",
+				"profile_sidebar_fill_color": "DDEEF6",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": false,
+				"default_profile": true,
+				"default_profile_image": false,
+				"following": true,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none",
+				"withheld_in_countries": []
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 0,
+			"favorite_count": 72,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 0,
+		"favorite_count": 4,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Whoooop! https://t.co/jtwXxFVzNp"
+	}
+]

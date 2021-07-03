589bb721df5516b467856f4f35467ca23afc6029
LeeFallin <lee@newgecko.net>
2021-07-03 01:21:16 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 10445,
-	"created_at": "Fri Jul 02 00:21:15 +0000 2021",
+	"tweets": 10446,
+	"created_at": "Sat Jul 03 00:21:15 +0000 2021",
 	"lang": "en-gb"
 }
--- data/js/tweets/2021_07.js
+++ data/js/tweets/2021_07.js
@@ -1,6 +1,139 @@
 Grailbird.data.tweets_2021_07 = 
 [
 	{
+		"created_at": "Fri Jul 02 19:22:41 +0000 2021",
+		"id": 1410906091408564200,
+		"id_str": "1411042664770854915",
+		"full_text": "An excellent introduction to the ethics of online research from @LeeFallin and @livinginhope   https://t.co/OBCW6QX4Hy via @YouTube \n#DigiResHull",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			145
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "DigiResHull",
+					"indices": [
+						133,
+						145
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "LeeFallin",
+					"name": "Dr Lee Fallin",
+					"id": 55448837,
+					"id_str": "55448837",
+					"indices": [
+						64,
+						74
+					]
+				},
+				{
+					"screen_name": "livinginhope",
+					"name": "Dr Jane Wray 🎗",
+					"id": 22756431,
+					"id_str": "22756431",
+					"indices": [
+						79,
+						92
+					]
+				},
+				{
+					"screen_name": "YouTube",
+					"name": "YouTube",
+					"id": 10228272,
+					"id_str": "10228272",
+					"indices": [
+						123,
+						131
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/OBCW6QX4Hy",
+					"expanded_url": "https://youtu.be/6tKlUl-Ia_Y",
+					"display_url": "youtu.be/6tKlUl-Ia_Y",
+					"indices": [
+						95,
+						118
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 740774701,
+			"id_str": "740774701",
+			"name": "Dr Laurie Dunn",
+			"screen_name": "l_dunn",
+			"location": "UK",
+			"description": "Post doctoral researcher in primary care and palliative care. Lover of bike rides and adventures.",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 120,
+			"friends_count": 345,
+			"listed_count": 5,
+			"created_at": "Mon Aug 06 14:47:54 +0000 2012",
+			"favourites_count": 93,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 60,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/443795186682826752/7I85_rXb_normal.jpeg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/443795186682826752/7I85_rXb_normal.jpeg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/740774701/1409754814",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 3,
+		"favorite_count": 4,
+		"favorited": true,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "An excellent introduction to the ethics of online research from @LeeFallin and @livinginhope   https://t.co/OBCW6QX4Hy via @YouTube \n#DigiResHull"
+	},
+	{
 		"created_at": "Thu Jul 01 14:38:04 +0000 2021",
 		"id": 1410608650863841300,
 		"id_str": "1410608650863841297",
@@ -38,7 +171,7 @@ Grailbird.data.tweets_2021_07 =
 			"name": "Dr Lee Fallin",
 			"screen_name": "LeeFallin",
 			"location": "United Kingdom",
-			"description": "SFHEA EdD FRGS 🏳️‍🌈 #LearningDeveloper, #MCE & #MIEExpert @UniofHull. I tweet #HigherEd, #EdTech, #Lego, #loveLD & #Hull. All tweets in a personal capacity",
+			"description": "SFHEA EdD FRGS 🏳️🌈 #LearningDeveloper, #MCE & #MIEExpert @UniofHull. I tweet #HigherEd, #EdTech, #Lego, #loveLD & #Hull. All tweets in a personal capacity",
 			"url": "https://t.co/UWrodKaQLd",
 			"entities": {
 				"url": {

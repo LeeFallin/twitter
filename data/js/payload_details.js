2d726d5684f4b29dee9eabba5c0ad37802aae432
LeeFallin <lee@newgecko.net>
2021-07-05 01:21:15 +0100

    Added 4 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 10446,
-	"created_at": "Sat Jul 03 00:21:15 +0000 2021",
+	"tweets": 10450,
+	"created_at": "Mon Jul 05 00:21:15 +0000 2021",
 	"lang": "en-gb"
 }
--- data/js/tweets/2021_07.js
+++ data/js/tweets/2021_07.js
@@ -1,6 +1,642 @@
 Grailbird.data.tweets_2021_07 = 
 [
 	{
+		"created_at": "Sun Jul 04 22:49:30 +0000 2021",
+		"id": 1411038005633163300,
+		"id_str": "1411819484294307847",
+		"full_text": "That’s one of my favourites too @raulpacheco with lots of great practical advice #DigiResHull https://t.co/B1MrFusznB",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			93
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "DigiResHull",
+					"indices": [
+						81,
+						93
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "raulpacheco",
+					"name": "Dr Raul Pacheco-Vega",
+					"id": 15432179,
+					"id_str": "15432179",
+					"indices": [
+						32,
+						44
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/B1MrFusznB",
+					"expanded_url": "https://twitter.com/nicki_research/status/1409957987234586628",
+					"display_url": "twitter.com/nicki_research…",
+					"indices": [
+						94,
+						117
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 22756431,
+			"id_str": "22756431",
+			"name": "Dr Jane Wray 🎗",
+			"screen_name": "livinginhope",
+			"location": "Hull",
+			"description": "Lives / works in Hull. Mum, Nurse, Partner, Director of Research Hull Univ, Hull City Fan (not necessarily in that order) enthusiastic supporter #Hull2017",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 1893,
+			"friends_count": 1011,
+			"listed_count": 81,
+			"created_at": "Wed Mar 04 10:46:46 +0000 2009",
+			"favourites_count": 39728,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 39708,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1173330403475828737/d7clMgmt_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1173330403475828737/d7clMgmt_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/22756431/1407749591",
+			"profile_link_color": "91D2FA",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1409957987234586600,
+		"quoted_status_id_str": "1409957987234586628",
+		"quoted_status_permalink": {
+			"url": "https://t.co/B1MrFusznB",
+			"expanded": "https://twitter.com/nicki_research/status/1409957987234586628",
+			"display": "twitter.com/nicki_research…"
+		},
+		"quoted_status": {
+			"created_at": "Tue Jun 29 19:32:34 +0000 2021",
+			"id": 1409957987234586600,
+			"id_str": "1409957987234586628",
+			"full_text": "Day 2 of the @UniOfHull Digital Student course. Love the idea of @ConversationUK as a place to read &amp; publish. My favourite academic blog is by @raulpacheco https://t.co/mIPJCDtg4d Great explanations, examples &amp; resouces. #DigiResHull https://t.co/IHQpr3o8YV",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				242
+			],
+			"entities": {
+				"hashtags": [
+					{
+						"text": "DigiResHull",
+						"indices": [
+							230,
+							242
+						]
+					}
+				],
+				"symbols": [],
+				"user_mentions": [
+					{
+						"screen_name": "UniOfHull",
+						"name": "University of Hull",
+						"id": 59761280,
+						"id_str": "59761280",
+						"indices": [
+							13,
+							23
+						]
+					},
+					{
+						"screen_name": "ConversationUK",
+						"name": "The Conversation",
+						"id": 1241258612,
+						"id_str": "1241258612",
+						"indices": [
+							65,
+							80
+						]
+					},
+					{
+						"screen_name": "raulpacheco",
+						"name": "Dr Raul Pacheco-Vega",
+						"id": 15432179,
+						"id_str": "15432179",
+						"indices": [
+							148,
+							160
+						]
+					}
+				],
+				"urls": [
+					{
+						"url": "https://t.co/mIPJCDtg4d",
+						"expanded_url": "http://www.raulpacheco.org/blog/",
+						"display_url": "raulpacheco.org/blog/",
+						"indices": [
+							161,
+							184
+						]
+					}
+				],
+				"media": [
+					{
+						"id": 1409957848499556400,
+						"id_str": "1409957848499556355",
+						"indices": [
+							243,
+							266
+						],
+						"media_url": "http://pbs.twimg.com/media/E5EtMWQXEAMstbW.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/E5EtMWQXEAMstbW.jpg",
+						"url": "https://t.co/IHQpr3o8YV",
+						"display_url": "pic.twitter.com/IHQpr3o8YV",
+						"expanded_url": "https://twitter.com/nicki_research/status/1409957987234586628/photo/1",
+						"type": "photo",
+						"sizes": {
+							"medium": {
+								"w": 1200,
+								"h": 969,
+								"resize": "fit"
+							},
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"large": {
+								"w": 2048,
+								"h": 1653,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 680,
+								"h": 549,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 1409957848499556400,
+						"id_str": "1409957848499556355",
+						"indices": [
+							243,
+							266
+						],
+						"media_url": "http://pbs.twimg.com/media/E5EtMWQXEAMstbW.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/E5EtMWQXEAMstbW.jpg",
+						"url": "https://t.co/IHQpr3o8YV",
+						"display_url": "pic.twitter.com/IHQpr3o8YV",
+						"expanded_url": "https://twitter.com/nicki_research/status/1409957987234586628/photo/1",
+						"type": "photo",
+						"sizes": {
+							"medium": {
+								"w": 1200,
+								"h": 969,
+								"resize": "fit"
+							},
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"large": {
+								"w": 2048,
+								"h": 1653,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 680,
+								"h": 549,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 238568127,
+				"id_str": "238568127",
+				"name": "Nicki Wise",
+				"screen_name": "nicki_research",
+				"location": "Edinburgh, Scotland",
+				"description": "Educator, Researcher, PhD student. Views my own.",
+				"url": null,
+				"entities": {
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 1812,
+				"friends_count": 2155,
+				"listed_count": 53,
+				"created_at": "Sat Jan 15 13:40:53 +0000 2011",
+				"favourites_count": 768,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": false,
+				"verified": false,
+				"statuses_count": 3345,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "FF6699",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme11/bg.gif",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme11/bg.gif",
+				"profile_background_tile": true,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/1181215948856016896/_oZuhL4T_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/1181215948856016896/_oZuhL4T_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/238568127/1607281682",
+				"profile_link_color": "B40B43",
+				"profile_sidebar_border_color": "CC3366",
+				"profile_sidebar_fill_color": "E5507E",
+				"profile_text_color": "362720",
+				"profile_use_background_image": true,
+				"has_extended_profile": false,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none",
+				"withheld_in_countries": []
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 3,
+			"favorite_count": 9,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 3,
+		"favorite_count": 9,
+		"favorited": true,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "That’s one of my favourites too @raulpacheco with lots of great practical advice #DigiResHull https://t.co/B1MrFusznB"
+	},
+	{
+		"created_at": "Sun Jul 04 22:49:09 +0000 2021",
+		"id": 1411076208331272200,
+		"id_str": "1411819398785024001",
+		"full_text": "I earned a badge from Digital Researcher! https://t.co/xC08hgNVQR \n\n#DigiResHull",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			80
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "DigiResHull",
+					"indices": [
+						68,
+						80
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/xC08hgNVQR",
+					"expanded_url": "https://api.eu.badgr.io/public/assertions/ArhDbzaWTH-_IjVeHl1mPQ",
+					"display_url": "api.eu.badgr.io/public/asserti…",
+					"indices": [
+						42,
+						65
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1007282035,
+			"id_str": "1007282035",
+			"name": "Rasmia",
+			"screen_name": "Rasmieh1",
+			"location": "Hull, England",
+			"description": "University of Hull\nPGR/ Nursing",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 18,
+			"friends_count": 114,
+			"listed_count": 1,
+			"created_at": "Wed Dec 12 20:46:18 +0000 2012",
+			"favourites_count": 96,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 19,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/2967345098/b59c403839b67a647f600921b2e55d1b_normal.jpeg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/2967345098/b59c403839b67a647f600921b2e55d1b_normal.jpeg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1007282035/1625269281",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 2,
+		"favorite_count": 3,
+		"favorited": true,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "I earned a badge from Digital Researcher! https://t.co/xC08hgNVQR \n\n#DigiResHull"
+	},
+	{
+		"created_at": "Sun Jul 04 22:49:05 +0000 2021",
+		"id": 1411095564696105000,
+		"id_str": "1411819382301409280",
+		"full_text": "Just completed #DigiResHull course and earned a badge!  https://t.co/dN9BvknlH7",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			79
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "DigiResHull",
+					"indices": [
+						15,
+						27
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/dN9BvknlH7",
+					"expanded_url": "https://api.eu.badgr.io/public/assertions/HOiq873ERI6ckeUTOKPMuA",
+					"display_url": "api.eu.badgr.io/public/asserti…",
+					"indices": [
+						56,
+						79
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1409423320848736300,
+			"id_str": "1409423320848736257",
+			"name": "Ethica Tanjeen",
+			"screen_name": "ETanjeen",
+			"location": "Hull, England",
+			"description": "PhD Student, Hull Business School, University of Hull",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 1,
+			"friends_count": 13,
+			"listed_count": 1,
+			"created_at": "Mon Jun 28 08:08:21 +0000 2021",
+			"favourites_count": 14,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 6,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1409625107195125765/xwu_GbDq_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1409625107195125765/xwu_GbDq_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1409423320848736257/1624916092",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 3,
+		"favorite_count": 3,
+		"favorited": true,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Just completed #DigiResHull course and earned a badge!  https://t.co/dN9BvknlH7"
+	},
+	{
+		"created_at": "Sun Jul 04 22:49:01 +0000 2021",
+		"id": 1411126650901774300,
+		"id_str": "1411819363259269122",
+		"full_text": "I earned a badge from Digital Researcher!🥳🤓https://t.co/K1CvfxiJYY Thanks for all the fun and very needed knowledge for the future endeavours #DigiResHull",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			154
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "DigiResHull",
+					"indices": [
+						142,
+						154
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/K1CvfxiJYY",
+					"expanded_url": "https://api.eu.badgr.io/public/assertions/Y5tidOhqTVmmaaIBimuC9Q",
+					"display_url": "api.eu.badgr.io/public/asserti…",
+					"indices": [
+						43,
+						66
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1311732748239933400,
+			"id_str": "1311732748239933440",
+			"name": "Roksana Malkowska-Michalak",
+			"screen_name": "RoksanaMM",
+			"location": "Kingston upon Hull",
+			"description": "A certified badass I Batman monomaniac I Postgraduate researcher I PhD criminology candidate I ESL teacher I Mental health activist",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 8,
+			"friends_count": 86,
+			"listed_count": 0,
+			"created_at": "Thu Oct 01 18:20:44 +0000 2020",
+			"favourites_count": 88,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 27,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1311733057293037568/DyNv0xlo_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1311733057293037568/DyNv0xlo_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1311732748239933440/1601577593",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 2,
+		"favorite_count": 3,
+		"favorited": true,
+		"retweeted": true,
+		"possibly_sensitive": true,
+		"lang": "en",
+		"text": "I earned a badge from Digital Researcher!🥳🤓https://t.co/K1CvfxiJYY Thanks for all the fun and very needed knowledge for the future endeavours #DigiResHull"
+	},
+	{
 		"created_at": "Fri Jul 02 19:22:41 +0000 2021",
 		"id": 1410906091408564200,
 		"id_str": "1411042664770854915",

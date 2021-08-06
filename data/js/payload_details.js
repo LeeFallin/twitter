1adc6bfd5a33c7670805dc1456e073cd4e2ca454
LeeFallin <lee@newgecko.net>
2021-08-06 01:21:15 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 10519,
-	"created_at": "Wed Aug 04 00:21:15 +0000 2021",
+	"tweets": 10520,
+	"created_at": "Fri Aug 06 00:21:15 +0000 2021",
 	"lang": "en-gb"
 }
--- data/js/tweets/2021_08.js
+++ data/js/tweets/2021_08.js
@@ -1,6 +1,140 @@
 Grailbird.data.tweets_2021_08 = 
 [
 	{
+		"created_at": "Thu Aug 05 11:03:07 +0000 2021",
+		"id": 1423191412661788700,
+		"id_str": "1423238129658515457",
+		"full_text": "Congratulations to @DominicCHenri and Neil Gordon announced as National Teaching Fellows today and to @UoHAcademy awarded a CATE for Collaborative Teaching Excellence @AdvanceHE @NTF_Tweet @UniOfHull",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			199
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "DominicCHenri",
+					"name": "Dominic Henri",
+					"id": 1685253343,
+					"id_str": "1685253343",
+					"indices": [
+						19,
+						33
+					]
+				},
+				{
+					"screen_name": "UoHAcademy",
+					"name": "Teaching Excellence Academy",
+					"id": 914481006,
+					"id_str": "914481006",
+					"indices": [
+						102,
+						113
+					]
+				},
+				{
+					"screen_name": "AdvanceHE",
+					"name": "Advance HE",
+					"id": 941587234465177600,
+					"id_str": "941587234465177600",
+					"indices": [
+						167,
+						177
+					]
+				},
+				{
+					"screen_name": "NTF_Tweet",
+					"name": "NTF Tweet",
+					"id": 2315738930,
+					"id_str": "2315738930",
+					"indices": [
+						178,
+						188
+					]
+				},
+				{
+					"screen_name": "UniOfHull",
+					"name": "University of Hull",
+					"id": 59761280,
+					"id_str": "59761280",
+					"indices": [
+						189,
+						199
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com/#!/download/ipad\" rel=\"nofollow\">Twitter for iPad</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 538778092,
+			"id_str": "538778092",
+			"name": "Graham Scott",
+			"screen_name": "GrahamScott14",
+			"location": "",
+			"description": "Professor of Bioscience Education, PFHEA, NTF, birder, ringer, and biologist. Editor of Ringing & Migration #ornithology. Watch the road not the birds!",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 1543,
+			"friends_count": 1033,
+			"listed_count": 64,
+			"created_at": "Wed Mar 28 06:34:01 +0000 2012",
+			"favourites_count": 36181,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 19968,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "ACDED6",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme18/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme18/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1302296002850942979/PuAkR3KS_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1302296002850942979/PuAkR3KS_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/538778092/1440840418",
+			"profile_link_color": "038543",
+			"profile_sidebar_border_color": "EEEEEE",
+			"profile_sidebar_fill_color": "F6F6F6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 8,
+		"favorite_count": 60,
+		"favorited": true,
+		"retweeted": true,
+		"lang": "en",
+		"text": "Congratulations to @DominicCHenri and Neil Gordon announced as National Teaching Fellows today and to @UoHAcademy awarded a CATE for Collaborative Teaching Excellence @AdvanceHE @NTF_Tweet @UniOfHull"
+	},
+	{
 		"created_at": "Tue Aug 03 15:41:16 +0000 2021",
 		"id": 1422583354218668000,
 		"id_str": "1422583354218668035",
@@ -39,7 +173,7 @@ Grailbird.data.tweets_2021_08 =
 			"name": "Dr Lee Fallin",
 			"screen_name": "LeeFallin",
 			"location": "United Kingdom",
-			"description": "SFHEA EdD FRGS 🏳️‍🌈 #LearningDeveloper, #MCE & #MIEExpert @UniofHull. I tweet #HigherEd, #EdTech, #Lego, #loveLD & #Hull. All tweets in a personal capacity",
+			"description": "SFHEA EdD FRGS 🏳️🌈 #LearningDeveloper, #MCE & #MIEExpert @UniofHull. I tweet #HigherEd, #EdTech, #Lego, #loveLD & #Hull. All tweets in a personal capacity",
 			"url": "https://t.co/UWrodKaQLd",
 			"entities": {
 				"url": {

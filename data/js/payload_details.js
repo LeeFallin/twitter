1ecb6787bf63410371051653ce3b9bfb8b51d51d
LeeFallin <lee@newgecko.net>
2021-10-03 01:21:16 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 10572,
-	"created_at": "Fri Oct 01 00:21:16 +0000 2021",
+	"tweets": 10573,
+	"created_at": "Sun Oct 03 00:21:15 +0000 2021",
 	"lang": "en-gb"
 }
---
+++ data/js/tweets/2021_10.js
@@ -0,0 +1,164 @@
+Grailbird.data.tweets_2021_10 = 
+[
+	{
+		"created_at": "Sat Oct 02 08:49:39 +0000 2021",
+		"id": 1444223037738475500,
+		"id_str": "1444223037738475522",
+		"full_text": "A few tweaks to one of my favourite articles:\nUsing LEGO as a teaching aid for academic writing at university. \n#LEGO #Teaching #HigherEd #LTHEchat #loveLD #MIEExpert @LEGO_Education \nhttps://t.co/Kv9Enn96Vz",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			207
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "LEGO",
+					"indices": [
+						112,
+						117
+					]
+				},
+				{
+					"text": "Teaching",
+					"indices": [
+						118,
+						127
+					]
+				},
+				{
+					"text": "HigherEd",
+					"indices": [
+						128,
+						137
+					]
+				},
+				{
+					"text": "LTHEchat",
+					"indices": [
+						138,
+						147
+					]
+				},
+				{
+					"text": "loveLD",
+					"indices": [
+						148,
+						155
+					]
+				},
+				{
+					"text": "MIEExpert",
+					"indices": [
+						156,
+						166
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "LEGO_Education",
+					"name": "LEGO Education",
+					"id": 50318149,
+					"id_str": "50318149",
+					"indices": [
+						167,
+						182
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/Kv9Enn96Vz",
+					"expanded_url": "https://leefallin.co.uk/2021/10/using-lego-as-a-teaching-aid-for-academic-essay-writing-in-higher-education/",
+					"display_url": "leefallin.co.uk/2021/10/using-…",
+					"indices": [
+						184,
+						207
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 55448837,
+			"id_str": "55448837",
+			"name": "Dr Lee Fallin",
+			"screen_name": "LeeFallin",
+			"location": "United Kingdom",
+			"description": "SFHEA EdD FRGS 🏳️‍🌈 #LearningDeveloper, #MCE & #MIEExpert @UniofHull. I tweet #HigherEd, #EdTech, #Lego, #loveLD & #Hull. All tweets in a personal capacity",
+			"url": "https://t.co/UWrodKaQLd",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/UWrodKaQLd",
+							"expanded_url": "http://www.leefallin.co.uk",
+							"display_url": "leefallin.co.uk",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 1460,
+			"friends_count": 1260,
+			"listed_count": 62,
+			"created_at": "Fri Jul 10 02:53:40 +0000 2009",
+			"favourites_count": 7742,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 10725,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/753894560108011520/7h68mawt_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/753894560108011520/7h68mawt_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/55448837/1555627842",
+			"profile_link_color": "1B95E0",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 2,
+		"favorite_count": 6,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "A few tweaks to one of my favourite articles:\nUsing LEGO as a teaching aid for academic writing at university. \n#LEGO #Teaching #HigherEd #LTHEchat #loveLD #MIEExpert @LEGO_Education \nhttps://t.co/Kv9Enn96Vz"
+	}
+]

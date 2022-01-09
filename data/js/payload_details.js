7cf39442ff2f11fc8223d03597b7aa3009758570
LeeFallin <lee@newgecko.net>
2022-01-09 00:21:17 +0000

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 10869,
-	"created_at": "Tue Jan 04 00:21:15 +0000 2022",
+	"tweets": 10870,
+	"created_at": "Sun Jan 09 00:21:16 +0000 2022",
 	"lang": "en-gb"
 }
--- data/js/tweets/2022_01.js
+++ data/js/tweets/2022_01.js
@@ -1,10 +1,125 @@
 Grailbird.data.tweets_2022_01 = 
 [
 	{
+		"created_at": "Sat Jan 08 11:55:33 +0000 2022",
+		"id": 1479438500852347000,
+		"id_str": "1479783833872605188",
+		"full_text": "We have such a great line-up of speakers at our Winter Symposium- Building &amp; Supporting Positive Learning Relationships \n\nBookings close on Tuesday: \nhttps://t.co/VUVS9Uocre \n\nSee you there!\n\n#TEAWinter2022",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			210
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "TEAWinter2022",
+					"indices": [
+						196,
+						210
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/VUVS9Uocre",
+					"expanded_url": "https://libcal.hull.ac.uk/event/3826457",
+					"display_url": "libcal.hull.ac.uk/event/3826457",
+					"indices": [
+						154,
+						177
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 914481006,
+			"id_str": "914481006",
+			"name": "Teaching Excellence Academy",
+			"screen_name": "UoHAcademy",
+			"location": "The University of Hull, UK",
+			"description": "Latest news from the team as we look to celebrate, develop and promote excellent teaching at @UniofHull.",
+			"url": "https://t.co/jpQpljYSrP",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/jpQpljYSrP",
+							"expanded_url": "http://hull.ac.uk/academy",
+							"display_url": "hull.ac.uk/academy",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 1109,
+			"friends_count": 765,
+			"listed_count": 19,
+			"created_at": "Tue Oct 30 12:02:43 +0000 2012",
+			"favourites_count": 1568,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 1882,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1142066391329230850/5iDA3Txv_normal.png",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1142066391329230850/5iDA3Txv_normal.png",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/914481006/1533221368",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 7,
+		"favorite_count": 11,
+		"favorited": true,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "We have such a great line-up of speakers at our Winter Symposium- Building &amp; Supporting Positive Learning Relationships \n\nBookings close on Tuesday: \nhttps://t.co/VUVS9Uocre \n\nSee you there!\n\n#TEAWinter2022"
+	},
+	{
 		"created_at": "Mon Jan 03 05:18:38 +0000 2022",
 		"id": 1477872005357703200,
 		"id_str": "1477872005357703169",
-		"full_text": "@SuzanneNye Same! It’s usually why I make a point with the pram. It is so inconsiderate and rude. When I used to live on Goddard it was awful - people would park across paths all the time. 🤦‍♂️",
+		"full_text": "@SuzanneNye Same! It’s usually why I make a point with the pram. It is so inconsiderate and rude. When I used to live on Goddard it was awful - people would park across paths all the time. 🤦♂️",
 		"truncated": false,
 		"display_text_range": [
 			12,
@@ -39,7 +154,7 @@ Grailbird.data.tweets_2022_01 =
 			"name": "Dr Lee Fallin",
 			"screen_name": "LeeFallin",
 			"location": "United Kingdom",
-			"description": "SFHEA EdD FRGS 🏳️‍🌈 #LearningDeveloper, #MCE & #MIEExpert @UniofHull. I tweet #HigherEd, #EdTech, #Lego, #loveLD & #Hull. All tweets in a personal capacity",
+			"description": "SFHEA EdD FRGS 🏳️🌈 #LearningDeveloper, #MCE & #MIEExpert @UniofHull. I tweet #HigherEd, #EdTech, #Lego, #loveLD & #Hull. All tweets in a personal capacity",
 			"url": "https://t.co/UWrodKaQLd",
 			"entities": {
 				"url": {
@@ -105,7 +220,7 @@ Grailbird.data.tweets_2022_01 =
 		"favorited": false,
 		"retweeted": false,
 		"lang": "en",
-		"text": "@SuzanneNye Same! It’s usually why I make a point with the pram. It is so inconsiderate and rude. When I used to live on Goddard it was awful - people would park across paths all the time. 🤦‍♂️"
+		"text": "@SuzanneNye Same! It’s usually why I make a point with the pram. It is so inconsiderate and rude. When I used to live on Goddard it was awful - people would park across paths all the time. 🤦♂️"
 	},
 	{
 		"created_at": "Sun Jan 02 17:50:34 +0000 2022",

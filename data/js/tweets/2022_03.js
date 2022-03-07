ed4028bf44cd710c1dbff6f5a48aa5ec1ffafa84
LeeFallin <lee@newgecko.net>
2022-03-07 00:21:16 +0000

    Added 1 tweets

--- data/js/tweets/2022_03.js
+++ data/js/tweets/2022_03.js
@@ -1,10 +1,204 @@
 Grailbird.data.tweets_2022_03 = 
 [
 	{
+		"created_at": "Sun Mar 06 02:04:04 +0000 2022",
+		"id": 1500239747255521300,
+		"id_str": "1500291090942939140",
+		"full_text": "@LeeFallin I saw this and thought of you https://t.co/9iED39CGNh",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			40
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "LeeFallin",
+					"name": "Dr Lee Fallin",
+					"id": 55448837,
+					"id_str": "55448837",
+					"indices": [
+						0,
+						10
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/9iED39CGNh",
+					"expanded_url": "https://twitter.com/schaalfan/status/1499802961320550410",
+					"display_url": "twitter.com/schaalfan/stat…",
+					"indices": [
+						41,
+						64
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": 55448837,
+		"in_reply_to_user_id_str": "55448837",
+		"in_reply_to_screen_name": "LeeFallin",
+		"user": {
+			"id": 2759706273,
+			"id_str": "2759706273",
+			"name": "Suzanne Blake",
+			"screen_name": "SuzanneNye",
+			"location": "",
+			"description": "",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 318,
+			"friends_count": 761,
+			"listed_count": 11,
+			"created_at": "Tue Sep 02 20:18:12 +0000 2014",
+			"favourites_count": 15679,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 12922,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1487717083907600387/WxTOmeXf_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1487717083907600387/WxTOmeXf_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/2759706273/1643534368",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1499802961320550400,
+		"quoted_status_id_str": "1499802961320550410",
+		"quoted_status_permalink": {
+			"url": "https://t.co/9iED39CGNh",
+			"expanded": "https://twitter.com/schaalfan/status/1499802961320550410",
+			"display": "twitter.com/schaalfan/stat…"
+		},
+		"quoted_status": {
+			"created_at": "Fri Mar 04 17:44:25 +0000 2022",
+			"id": 1499802961320550400,
+			"id_str": "1499802961320550410",
+			"full_text": "Today a woman with developmental disabilities came into the library, and said she was lost. She didn't know her address, but her phone number was in her pocket on a piece of paper with Elmo on it. She kept saying, \"The library is a safe place.\"",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				244
+			],
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": []
+			},
+			"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 295455365,
+				"id_str": "295455365",
+				"name": "David",
+				"screen_name": "schaalfan",
+				"location": "Atlanta, GA",
+				"description": "Librarian. Writer. Still missing Party Down. Views/tweets are my own and don’t represent my employer.",
+				"url": null,
+				"entities": {
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 4925,
+				"friends_count": 1534,
+				"listed_count": 40,
+				"created_at": "Mon May 09 02:09:20 +0000 2011",
+				"favourites_count": 12779,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": false,
+				"verified": false,
+				"statuses_count": 4269,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "C0DEED",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/1484523637805928456/5GHjT04g_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/1484523637805928456/5GHjT04g_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/295455365/1627091671",
+				"profile_link_color": "1DA1F2",
+				"profile_sidebar_border_color": "C0DEED",
+				"profile_sidebar_fill_color": "DDEEF6",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": false,
+				"default_profile": true,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none",
+				"withheld_in_countries": []
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 17746,
+			"favorite_count": 267815,
+			"favorited": false,
+			"retweeted": false,
+			"lang": "en"
+		},
+		"retweet_count": 1,
+		"favorite_count": 1,
+		"favorited": true,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "@LeeFallin I saw this and thought of you https://t.co/9iED39CGNh"
+	},
+	{
 		"created_at": "Tue Mar 01 19:45:23 +0000 2022",
 		"id": 1498746240930136000,
 		"id_str": "1498746240930136071",
-		"full_text": "Oh dear... tried to do some last minute student-ing - but didn't realise the discussion closed last week. Sorry @GrahamScott14 🙇‍♂️",
+		"full_text": "Oh dear... tried to do some last minute student-ing - but didn't realise the discussion closed last week. Sorry @GrahamScott14 🙇♂️",
 		"truncated": false,
 		"display_text_range": [
 			0,
@@ -39,7 +233,7 @@ Grailbird.data.tweets_2022_03 =
 			"name": "Dr Lee Fallin",
 			"screen_name": "LeeFallin",
 			"location": "United Kingdom",
-			"description": "SFHEA EdD FRGS 🏳️‍🌈 #LearningDeveloper, #MCE & #MIEExpert @UniofHull. I tweet #HigherEd, #EdTech, #Lego, #loveLD & #Hull. All tweets in a personal capacity",
+			"description": "SFHEA EdD FRGS 🏳️🌈 #LearningDeveloper, #MCE & #MIEExpert @UniofHull. I tweet #HigherEd, #EdTech, #Lego, #loveLD & #Hull. All tweets in a personal capacity",
 			"url": "https://t.co/UWrodKaQLd",
 			"entities": {
 				"url": {
@@ -105,6 +299,6 @@ Grailbird.data.tweets_2022_03 =
 		"favorited": false,
 		"retweeted": false,
 		"lang": "en",
-		"text": "Oh dear... tried to do some last minute student-ing - but didn't realise the discussion closed last week. Sorry @GrahamScott14 🙇‍♂️"
+		"text": "Oh dear... tried to do some last minute student-ing - but didn't realise the discussion closed last week. Sorry @GrahamScott14 🙇♂️"
 	}
 ]

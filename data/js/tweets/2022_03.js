aa0e993198b397d8608fcd928b5678e21e5502e1
LeeFallin <lee@newgecko.net>
2022-03-08 00:21:15 +0000

    Added 1 tweets

--- data/js/tweets/2022_03.js
+++ data/js/tweets/2022_03.js
@@ -1,6 +1,102 @@
 Grailbird.data.tweets_2022_03 = 
 [
 	{
+		"created_at": "Mon Mar 07 18:32:07 +0000 2022",
+		"id": 1500529757825417200,
+		"id_str": "1500902127522172934",
+		"full_text": "I am a proofreader. Would you like a list of words which are very nearly, not not quite, the same? \n\nOf course you would 😆\nAll right… 🧵",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			135
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 14803486,
+			"id_str": "14803486",
+			"name": "Dr Kat Day 🏳‍🌈 🧪🐙 🇺🇦",
+			"screen_name": "chronicleflask",
+			"location": "Oxfordshire",
+			"description": "Science & Fiction editor/writer. Chemistry PhD. Writer @TheCrashCourse Organic Chemistry. Asst Ed @PseudoPod_org. ChronicleFlask & FictionPhial blogs. She/her",
+			"url": "https://t.co/CABhH3b4wi",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/CABhH3b4wi",
+							"expanded_url": "https://linktr.ee/katLday",
+							"display_url": "linktr.ee/katLday",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 15365,
+			"friends_count": 936,
+			"listed_count": 303,
+			"created_at": "Fri May 16 20:11:26 +0000 2008",
+			"favourites_count": 65341,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 39906,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1468621419567321099/l-rn8Q6R_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1468621419567321099/l-rn8Q6R_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/14803486/1594230821",
+			"profile_link_color": "E6004C",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 28063,
+		"favorite_count": 149726,
+		"favorited": true,
+		"retweeted": true,
+		"lang": "en",
+		"text": "I am a proofreader. Would you like a list of words which are very nearly, not not quite, the same? \n\nOf course you would 😆\nAll right… 🧵"
+	},
+	{
 		"created_at": "Sun Mar 06 02:04:04 +0000 2022",
 		"id": 1500239747255521300,
 		"id_str": "1500291090942939140",

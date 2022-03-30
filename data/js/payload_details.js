de0db365056ec06502de5c8a5469b2a1dae489bd
LeeFallin <lee@newgecko.net>
2022-03-30 01:21:18 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 10922,
-	"created_at": "Sun Mar 20 00:21:15 +0000 2022",
+	"tweets": 10923,
+	"created_at": "Wed Mar 30 00:21:16 +0000 2022",
 	"lang": "en-gb"
 }
--- data/js/tweets/2022_03.js
+++ data/js/tweets/2022_03.js
@@ -1,6 +1,113 @@
 Grailbird.data.tweets_2022_03 = 
 [
 	{
+		"created_at": "Tue Mar 29 09:33:13 +0000 2022",
+		"id": 1508436231767703600,
+		"id_str": "1508739044196425728",
+		"full_text": "Great review of this timely book on 3rd space practitioners in HE- I have a chapter in it, and there’s plenty of other fantastic contributions I can’t wait to read! https://t.co/EdrGG44xmT",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			188
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/EdrGG44xmT",
+					"expanded_url": "https://wonkhe.com/blogs/book-review-the-impact-of-the-integrated-practitioner-in-higher-education-by-emily-mcintosh-and-dianne-nutt/?utm_medium=email&utm_campaign=Wonkhe%20Mondays%20-%2028%20March&utm_content=Wonkhe%20Mondays%20-%2028%20March+CID_297a9b1beefabc0471c9d4838092db90&utm_source=Email%20marketing%20software&utm_term=a%20new%20edited%20collection%20exploring%20the%20boundary-crossing%20third%20space%20professional%20roles%20in%20higher%20education",
+					"display_url": "wonkhe.com/blogs/book-rev…",
+					"indices": [
+						165,
+						188
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 395277106,
+			"id_str": "395277106",
+			"name": "Dr Helen Webster 📚🐀",
+			"screen_name": "scholastic_rat",
+			"location": "Newcastle Upon Tyne, England",
+			"description": "AdvanceHE Senior Adviser for Student Success. Academic Literacies and Learning Development, wellbeing and social justice in HE (NTF, CeLP, SFHEA)",
+			"url": "https://t.co/R1BsDA3Aab",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/R1BsDA3Aab",
+							"expanded_url": "https://rattusscholasticus.wordpress.com/",
+							"display_url": "rattusscholasticus.wordpress.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 2510,
+			"friends_count": 1364,
+			"listed_count": 79,
+			"created_at": "Fri Oct 21 12:25:28 +0000 2011",
+			"favourites_count": 17905,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 15661,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme3/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme3/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/502149725437833216/Tsu9iOGP_normal.jpeg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/502149725437833216/Tsu9iOGP_normal.jpeg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/395277106/1363797872",
+			"profile_link_color": "ABB8C2",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 8,
+		"favorite_count": 19,
+		"favorited": true,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Great review of this timely book on 3rd space practitioners in HE- I have a chapter in it, and there’s plenty of other fantastic contributions I can’t wait to read! https://t.co/EdrGG44xmT"
+	},
+	{
 		"created_at": "Sat Mar 19 08:14:58 +0000 2022",
 		"id": 1504882468305973200,
 		"id_str": "1505095472746311680",

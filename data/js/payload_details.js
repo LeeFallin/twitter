423d8810091eb58dd13429f31302983d306e0407
LeeFallin <lee@newgecko.net>
2022-04-02 01:21:15 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 10925,
-	"created_at": "Fri Apr 01 00:21:15 +0000 2022",
+	"tweets": 10926,
+	"created_at": "Sat Apr 02 00:21:15 +0000 2022",
 	"lang": "en-gb"
 }
---
+++ data/js/tweets/2022_04.js
@@ -0,0 +1,214 @@
+Grailbird.data.tweets_2022_04 = 
+[
+	{
+		"created_at": "Fri Apr 01 08:50:48 +0000 2022",
+		"id": 1509814086200086500,
+		"id_str": "1509815533264420896",
+		"full_text": "Customer Reminder: Pets are not allowed in the Library. If you are the owner of the horse - please remove it now. https://t.co/2IcBAKFQPR",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			113
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1509814080521093000,
+					"id_str": "1509814080521093132",
+					"indices": [
+						114,
+						137
+					],
+					"media_url": "http://pbs.twimg.com/media/FPPv6CpXwAw7fi_.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FPPv6CpXwAw7fi_.jpg",
+					"url": "https://t.co/2IcBAKFQPR",
+					"display_url": "pic.twitter.com/2IcBAKFQPR",
+					"expanded_url": "https://twitter.com/HullUni_Library/status/1509814086200086529/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 860,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1467,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 487,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1509814080521093000,
+					"id_str": "1509814080521093132",
+					"indices": [
+						114,
+						137
+					],
+					"media_url": "http://pbs.twimg.com/media/FPPv6CpXwAw7fi_.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FPPv6CpXwAw7fi_.jpg",
+					"url": "https://t.co/2IcBAKFQPR",
+					"display_url": "pic.twitter.com/2IcBAKFQPR",
+					"expanded_url": "https://twitter.com/HullUni_Library/status/1509814086200086529/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 860,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1467,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 487,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1509814080638533600,
+					"id_str": "1509814080638533632",
+					"indices": [
+						114,
+						137
+					],
+					"media_url": "http://pbs.twimg.com/media/FPPv6DFXwAAnXgO.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FPPv6DFXwAAnXgO.jpg",
+					"url": "https://t.co/2IcBAKFQPR",
+					"display_url": "pic.twitter.com/2IcBAKFQPR",
+					"expanded_url": "https://twitter.com/HullUni_Library/status/1509814086200086529/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 860,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1467,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 487,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://www.hootsuite.com\" rel=\"nofollow\">Hootsuite Inc.</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 239865033,
+			"id_str": "239865033",
+			"name": "Hull Uni Library",
+			"screen_name": "HullUni_Library",
+			"location": "Hull, East Yorkshire, UK",
+			"description": "News, events, training, information, new resources and alerts from the Brynmor Jones Library, at The University of Hull.",
+			"url": "https://t.co/TbBJdYe4pw",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/TbBJdYe4pw",
+							"expanded_url": "http://www.hull.ac.uk/lib",
+							"display_url": "hull.ac.uk/lib",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 7089,
+			"friends_count": 906,
+			"listed_count": 84,
+			"created_at": "Tue Jan 18 16:18:18 +0000 2011",
+			"favourites_count": 3827,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 10333,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "CB812A",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/894580794018209794/VVaF0eRS_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/894580794018209794/VVaF0eRS_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/239865033/1469185125",
+			"profile_link_color": "CB812A",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1,
+		"favorite_count": 18,
+		"favorited": true,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Customer Reminder: Pets are not allowed in the Library. If you are the owner of the horse - please remove it now. https://t.co/2IcBAKFQPR"
+	}
+]

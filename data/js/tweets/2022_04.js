b39981304af3348c586179ca163f6f58978aef21
LeeFallin <lee@newgecko.net>
2022-04-05 01:21:16 +0100

    Added 3 tweets

--- data/js/tweets/2022_04.js
+++ data/js/tweets/2022_04.js
@@ -1,6 +1,468 @@
 Grailbird.data.tweets_2022_04 = 
 [
 	{
+		"created_at": "Mon Apr 04 10:24:35 +0000 2022",
+		"id": 1510903451689136000,
+		"id_str": "1510926298725732352",
+		"full_text": "New YouTube video on appropriate use of quotation.\nhttps://t.co/tZPP3KpdJT\nOf interest?\n@IdreesRasouli @dlhampton @LeeFallin  \n@gabs_finn @MargyMacLibrary @MHmattersSOS  \n@MikeTylerSport @LauraJks2015 @__TweetinChar__  \n@DrLeeCoutts @SChewPsych @ZorynaODonnell \n@drlwhelen",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			272
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "IdreesRasouli",
+					"name": "Idrees Rasouli",
+					"id": 2228175109,
+					"id_str": "2228175109",
+					"indices": [
+						88,
+						102
+					]
+				},
+				{
+					"screen_name": "dlhampton",
+					"name": "Debbie Hampton",
+					"id": 16275858,
+					"id_str": "16275858",
+					"indices": [
+						103,
+						113
+					]
+				},
+				{
+					"screen_name": "LeeFallin",
+					"name": "Dr Lee Fallin",
+					"id": 55448837,
+					"id_str": "55448837",
+					"indices": [
+						114,
+						124
+					]
+				},
+				{
+					"screen_name": "gabs_finn",
+					"name": "Prof Gabrielle Finn",
+					"id": 2248968164,
+					"id_str": "2248968164",
+					"indices": [
+						127,
+						137
+					]
+				},
+				{
+					"screen_name": "MargyMacLibrary",
+					"name": "margy maclibrary",
+					"id": 381743098,
+					"id_str": "381743098",
+					"indices": [
+						138,
+						154
+					]
+				},
+				{
+					"screen_name": "MHmattersSOS",
+					"name": "Mental Health Matters",
+					"id": 240193193,
+					"id_str": "240193193",
+					"indices": [
+						155,
+						168
+					]
+				},
+				{
+					"screen_name": "MikeTylerSport",
+					"name": "Mike Tyler ن",
+					"id": 339031743,
+					"id_str": "339031743",
+					"indices": [
+						171,
+						186
+					]
+				},
+				{
+					"screen_name": "LauraJks2015",
+					"name": "Dr Laura Jenkins",
+					"id": 2411332987,
+					"id_str": "2411332987",
+					"indices": [
+						187,
+						200
+					]
+				},
+				{
+					"screen_name": "__TweetinChar__",
+					"name": ".CharlotteS.NaN.🇺🇦",
+					"id": 2919723412,
+					"id_str": "2919723412",
+					"indices": [
+						201,
+						217
+					]
+				},
+				{
+					"screen_name": "DrLeeCoutts",
+					"name": "Dr Lee Coutts",
+					"id": 783659789008199700,
+					"id_str": "783659789008199682",
+					"indices": [
+						220,
+						232
+					]
+				},
+				{
+					"screen_name": "SChewPsych",
+					"name": "Stephen Chew",
+					"id": 2273990546,
+					"id_str": "2273990546",
+					"indices": [
+						233,
+						244
+					]
+				},
+				{
+					"screen_name": "ZorynaODonnell",
+					"name": "Zoryna O'Donnell",
+					"id": 788057632535896000,
+					"id_str": "788057632535896064",
+					"indices": [
+						245,
+						260
+					]
+				},
+				{
+					"screen_name": "drlwhelen",
+					"name": "Dr Liz Whelen",
+					"id": 2229274389,
+					"id_str": "2229274389",
+					"indices": [
+						262,
+						272
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/tZPP3KpdJT",
+					"expanded_url": "https://www.youtube.com/watch?v=9CM3p8WQL6M&t=14s",
+					"display_url": "youtube.com/watch?v=9CM3p8…",
+					"indices": [
+						51,
+						74
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1126081823191326700,
+			"id_str": "1126081823191326720",
+			"name": "Dr. Paul Penn 💙",
+			"screen_name": "Dr_Paul_Penn",
+			"location": "London, England",
+			"description": "Psychology Teacher of The Year 2021 & Author of 'The Psychology of Effective Studying'. YouTube channel: https://t.co/U4mMOXjjrR…",
+			"url": "https://t.co/eSjUBI1ijT",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/eSjUBI1ijT",
+							"expanded_url": "https://www.drpaulpenn.com/",
+							"display_url": "drpaulpenn.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/U4mMOXjjrR",
+							"expanded_url": "http://youtube.com/psychologyofef",
+							"display_url": "youtube.com/psychologyofef",
+							"indices": [
+								105,
+								128
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 1963,
+			"friends_count": 2377,
+			"listed_count": 25,
+			"created_at": "Wed May 08 11:10:03 +0000 2019",
+			"favourites_count": 2014,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 2455,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1477651716443095040/marFC6rp_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1477651716443095040/marFC6rp_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1126081823191326720/1567508029",
+			"profile_link_color": "1B95E0",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 6,
+		"favorite_count": 8,
+		"favorited": true,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "New YouTube video on appropriate use of quotation.\nhttps://t.co/tZPP3KpdJT\nOf interest?\n@IdreesRasouli @dlhampton @LeeFallin  \n@gabs_finn @MargyMacLibrary @MHmattersSOS  \n@MikeTylerSport @LauraJks2015 @__TweetinChar__  \n@DrLeeCoutts @SChewPsych @ZorynaODonnell \n@drlwhelen"
+	},
+	{
+		"created_at": "Mon Apr 04 08:22:19 +0000 2022",
+		"id": 1510895525511499800,
+		"id_str": "1510895525511499776",
+		"full_text": "@Volcanologist …I do worry about the environmental cost of all these theme days and costumes though. \n\nNo chance of us getting them to wear the things we made for maths day again 🤣",
+		"truncated": false,
+		"display_text_range": [
+			15,
+			180
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "Volcanologist",
+					"name": "Dr Rebecca Williams",
+					"id": 217005773,
+					"id_str": "217005773",
+					"indices": [
+						0,
+						14
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": 1510895335383744500,
+		"in_reply_to_status_id_str": "1510895335383744519",
+		"in_reply_to_user_id": 55448837,
+		"in_reply_to_user_id_str": "55448837",
+		"in_reply_to_screen_name": "LeeFallin",
+		"user": {
+			"id": 55448837,
+			"id_str": "55448837",
+			"name": "Dr Lee Fallin",
+			"screen_name": "LeeFallin",
+			"location": "United Kingdom",
+			"description": "SFHEA EdD FRGS 🏳️‍🌈 #LearningDeveloper, #MCE & #MIEExpert @UniofHull. I tweet #HigherEd, #EdTech, #Lego, #loveLD & #Hull. All tweets in a personal capacity",
+			"url": "https://t.co/UWrodKaQLd",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/UWrodKaQLd",
+							"expanded_url": "http://www.leefallin.co.uk",
+							"display_url": "leefallin.co.uk",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 1536,
+			"friends_count": 1311,
+			"listed_count": 63,
+			"created_at": "Fri Jul 10 02:53:40 +0000 2009",
+			"favourites_count": 8295,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 11126,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/753894560108011520/7h68mawt_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/753894560108011520/7h68mawt_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/55448837/1555627842",
+			"profile_link_color": "1B95E0",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "en",
+		"text": "@Volcanologist …I do worry about the environmental cost of all these theme days and costumes though. \n\nNo chance of us getting them to wear the things we made for maths day again 🤣"
+	},
+	{
+		"created_at": "Mon Apr 04 08:21:33 +0000 2022",
+		"id": 1510895335383744500,
+		"id_str": "1510895335383744519",
+		"full_text": "@Volcanologist Technically - only needed two. But the youngest would have felt left out so everyone got one! \n\nI’m just so happy Asda had the base hats which saved a lot of time…",
+		"truncated": false,
+		"display_text_range": [
+			15,
+			178
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "Volcanologist",
+					"name": "Dr Rebecca Williams",
+					"id": 217005773,
+					"id_str": "217005773",
+					"indices": [
+						0,
+						14
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": 1510703258129555500,
+		"in_reply_to_status_id_str": "1510703258129555460",
+		"in_reply_to_user_id": 217005773,
+		"in_reply_to_user_id_str": "217005773",
+		"in_reply_to_screen_name": "Volcanologist",
+		"user": {
+			"id": 55448837,
+			"id_str": "55448837",
+			"name": "Dr Lee Fallin",
+			"screen_name": "LeeFallin",
+			"location": "United Kingdom",
+			"description": "SFHEA EdD FRGS 🏳️‍🌈 #LearningDeveloper, #MCE & #MIEExpert @UniofHull. I tweet #HigherEd, #EdTech, #Lego, #loveLD & #Hull. All tweets in a personal capacity",
+			"url": "https://t.co/UWrodKaQLd",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/UWrodKaQLd",
+							"expanded_url": "http://www.leefallin.co.uk",
+							"display_url": "leefallin.co.uk",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 1536,
+			"friends_count": 1311,
+			"listed_count": 63,
+			"created_at": "Fri Jul 10 02:53:40 +0000 2009",
+			"favourites_count": 8295,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 11126,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/753894560108011520/7h68mawt_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/753894560108011520/7h68mawt_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/55448837/1555627842",
+			"profile_link_color": "1B95E0",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "en",
+		"text": "@Volcanologist Technically - only needed two. But the youngest would have felt left out so everyone got one! \n\nI’m just so happy Asda had the base hats which saved a lot of time…"
+	},
+	{
 		"created_at": "Fri Apr 01 08:50:48 +0000 2022",
 		"id": 1509814086200086500,
 		"id_str": "1509815533264420896",

72bed0744a45abf6584f2b3744dd5d7b57396636
LeeFallin <lee@newgecko.net>
2022-10-02 01:21:17 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 11276,
-	"created_at": "Sat Oct 01 00:21:16 +0000 2022",
+	"tweets": 11277,
+	"created_at": "Sun Oct 02 00:21:16 +0000 2022",
 	"lang": "en-gb"
 }
---
+++ data/js/tweets/2022_10.js
@@ -0,0 +1,211 @@
+Grailbird.data.tweets_2022_10 = 
+[
+	{
+		"created_at": "Sat Oct 01 08:21:01 +0000 2022",
+		"id": 1576092003162816500,
+		"id_str": "1576125015992733696",
+		"full_text": "So true https://t.co/e49lVWyn7w",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			7
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1576091998863376400,
+					"id_str": "1576091998863376384",
+					"indices": [
+						8,
+						31
+					],
+					"media_url": "http://pbs.twimg.com/media/Fd9nUo2WQAAfRXX.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Fd9nUo2WQAAfRXX.jpg",
+					"url": "https://t.co/e49lVWyn7w",
+					"display_url": "pic.twitter.com/e49lVWyn7w",
+					"expanded_url": "https://twitter.com/FieryRed1/status/1576092003162816512/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1080,
+							"h": 1080,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1080,
+							"h": 1080,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1576091998863376400,
+					"id_str": "1576091998863376384",
+					"indices": [
+						8,
+						31
+					],
+					"media_url": "http://pbs.twimg.com/media/Fd9nUo2WQAAfRXX.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Fd9nUo2WQAAfRXX.jpg",
+					"url": "https://t.co/e49lVWyn7w",
+					"display_url": "pic.twitter.com/e49lVWyn7w",
+					"expanded_url": "https://twitter.com/FieryRed1/status/1576092003162816512/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1080,
+							"h": 1080,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1080,
+							"h": 1080,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 39770645,
+			"id_str": "39770645",
+			"name": "Fiery Red",
+			"screen_name": "FieryRed1",
+			"location": "Virtual",
+			"description": "Offering consultancy to the educational community.",
+			"url": "https://t.co/R9rh0cOxkK",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/R9rh0cOxkK",
+							"expanded_url": "https://fieryred.co.uk",
+							"display_url": "fieryred.co.uk",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 600,
+			"friends_count": 1881,
+			"listed_count": 16,
+			"created_at": "Wed May 13 15:21:23 +0000 2009",
+			"favourites_count": 10606,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 10135,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1326768132216791045/DyOhFi8-_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1326768132216791045/DyOhFi8-_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/39770645/1561973971",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": {
+			"id": "7d7bdec12d2549d4",
+			"url": "https://api.twitter.com/1.1/geo/id/7d7bdec12d2549d4.json",
+			"place_type": "city",
+			"name": "Nottingham",
+			"full_name": "Nottingham, England",
+			"country_code": "GB",
+			"country": "United Kingdom",
+			"contained_within": [],
+			"bounding_box": {
+				"type": "Polygon",
+				"coordinates": [
+					[
+						[
+							-1.2501363,
+							52.921473
+						],
+						[
+							-1.094396,
+							52.921473
+						],
+						[
+							-1.094396,
+							53.017965
+						],
+						[
+							-1.2501363,
+							53.017965
+						]
+					]
+				]
+			},
+			"attributes": {}
+		},
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 2,
+		"favorite_count": 10,
+		"favorited": true,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "So true https://t.co/e49lVWyn7w"
+	}
+]

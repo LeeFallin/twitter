eb568f88ab5116779182ba10e99f01f8c3e38a4d
LeeFallin <lee@newgecko.net>
2022-11-20 00:21:17 +0000

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 11396,
-	"created_at": "Mon Nov 14 00:21:16 +0000 2022",
+	"tweets": 11397,
+	"created_at": "Sun Nov 20 00:21:16 +0000 2022",
 	"lang": "en-gb"
 }

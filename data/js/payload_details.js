c834cf306aaacdd3109f46717c20e079c1f5f28c
LeeFallin <lee@newgecko.net>
2023-05-03 01:21:17 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 12152,
-	"created_at": "Sun Apr 30 00:21:17 +0000 2023",
+	"tweets": 12153,
+	"created_at": "Wed May 03 00:21:16 +0000 2023",
 	"lang": "en-gb"
 }
---
+++ data/js/tweets/2023_05.js
@@ -0,0 +1,316 @@
+Grailbird.data.tweets_2023_05 = 
+[
+	{
+		"created_at": "Tue May 02 22:55:43 +0000 2023",
+		"id": 1653400566905688000,
+		"id_str": "1653533757637263361",
+		"full_text": "@UniofReading Creating Accessible Content: Digital accessibility tips to make content work for all based on @LeeFallin &amp; @suewatling. Part of the Look Again campaign with @PeterGlen17 https://t.co/vIcslKM2WR https://t.co/wtfoWlm5jw",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			211
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "UniofReading",
+					"name": "Uni of Reading",
+					"id": 59142515,
+					"id_str": "59142515",
+					"indices": [
+						0,
+						13
+					]
+				},
+				{
+					"screen_name": "LeeFallin",
+					"name": "Dr Lee Fallin",
+					"id": 55448837,
+					"id_str": "55448837",
+					"indices": [
+						108,
+						118
+					]
+				},
+				{
+					"screen_name": "suewatling",
+					"name": "Sue Watling",
+					"id": 14897464,
+					"id_str": "14897464",
+					"indices": [
+						125,
+						136
+					]
+				},
+				{
+					"screen_name": "PeterGlen17",
+					"name": "Pete Bryant",
+					"id": 165879462,
+					"id_str": "165879462",
+					"indices": [
+						175,
+						187
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/vIcslKM2WR",
+					"expanded_url": "https://www.reading.ac.uk/digital-accessibility/resources/poster",
+					"display_url": "reading.ac.uk/digital-access…",
+					"indices": [
+						188,
+						211
+					]
+				},
+				{
+					"url": "https://t.co/wtfoWlm5jw",
+					"expanded_url": "https://twitter.com/VLEguru/status/1653397242970091522",
+					"display_url": "twitter.com/VLEguru/status…",
+					"indices": [
+						212,
+						235
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": 59142515,
+		"in_reply_to_user_id_str": "59142515",
+		"in_reply_to_screen_name": "UniofReading",
+		"user": {
+			"id": 1580531887,
+			"id_str": "1580531887",
+			"name": "Tharindu",
+			"screen_name": "Tharindu__",
+			"location": "Reading",
+			"description": "I'm interested in the social implications of ICTs. Digital Accessibility. The views expressed are my own & in no way reflect the views of the Uni of Reading",
+			"url": "https://t.co/6siIJM2WMU",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/6siIJM2WMU",
+							"expanded_url": "http://www.linkedin.com/in/tharindu-liyanagunawardena",
+							"display_url": "linkedin.com/in/tharindu-li…",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 459,
+			"friends_count": 340,
+			"listed_count": 25,
+			"created_at": "Tue Jul 09 14:24:20 +0000 2013",
+			"favourites_count": 1268,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 3354,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1458714582369853442/j2ZypS4M_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1458714582369853442/j2ZypS4M_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1580531887/1398343529",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1653397242970091500,
+		"quoted_status_id_str": "1653397242970091522",
+		"quoted_status_permalink": {
+			"url": "https://t.co/wtfoWlm5jw",
+			"expanded": "https://twitter.com/VLEguru/status/1653397242970091522",
+			"display": "twitter.com/VLEguru/status…"
+		},
+		"quoted_status": {
+			"created_at": "Tue May 02 13:53:16 +0000 2023",
+			"id": 1653397242970091500,
+			"id_str": "1653397242970091522",
+			"full_text": "Love this digital #accessibility poster by the @UniofReading Digital Accessibility Team!\n\nhttps://t.co/nbsuIIDrbh\n\n#EdTech\n#a11y \n@Tharindu__",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				141
+			],
+			"entities": {
+				"hashtags": [
+					{
+						"text": "accessibility",
+						"indices": [
+							18,
+							32
+						]
+					},
+					{
+						"text": "EdTech",
+						"indices": [
+							115,
+							122
+						]
+					},
+					{
+						"text": "a11y",
+						"indices": [
+							123,
+							128
+						]
+					}
+				],
+				"symbols": [],
+				"user_mentions": [
+					{
+						"screen_name": "UniofReading",
+						"name": "Uni of Reading",
+						"id": 59142515,
+						"id_str": "59142515",
+						"indices": [
+							47,
+							60
+						]
+					},
+					{
+						"screen_name": "Tharindu__",
+						"name": "Tharindu",
+						"id": 1580531887,
+						"id_str": "1580531887",
+						"indices": [
+							130,
+							141
+						]
+					}
+				],
+				"urls": [
+					{
+						"url": "https://t.co/nbsuIIDrbh",
+						"expanded_url": "https://www.reading.ac.uk/digital-accessibility/resources/poster",
+						"display_url": "reading.ac.uk/digital-access…",
+						"indices": [
+							90,
+							113
+						]
+					}
+				]
+			},
+			"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 3147567066,
+				"id_str": "3147567066",
+				"name": "Matthew Deeprose",
+				"screen_name": "VLEguru",
+				"location": "Southampton",
+				"description": "Senior Learning Designer in the University of Southampton @learnsoton team.  \nMastodon: @vleguru@oldbytes.space",
+				"url": "https://t.co/G3O6MPkR6m",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/G3O6MPkR6m",
+								"expanded_url": "https://matthewdeeprose.github.io/",
+								"display_url": "matthewdeeprose.github.io",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 1161,
+				"friends_count": 3105,
+				"listed_count": 18,
+				"created_at": "Fri Apr 10 08:37:24 +0000 2015",
+				"favourites_count": 2471,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": false,
+				"verified": false,
+				"statuses_count": 2235,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "000000",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/1126028547687100416/6UHNLpmx_normal.png",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/1126028547687100416/6UHNLpmx_normal.png",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/3147567066/1575037716",
+				"profile_link_color": "ABB8C2",
+				"profile_sidebar_border_color": "000000",
+				"profile_sidebar_fill_color": "000000",
+				"profile_text_color": "000000",
+				"profile_use_background_image": false,
+				"has_extended_profile": true,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": true,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none",
+				"withheld_in_countries": []
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 3,
+			"favorite_count": 6,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 1,
+		"favorite_count": 4,
+		"favorited": true,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "@UniofReading Creating Accessible Content: Digital accessibility tips to make content work for all based on @LeeFallin &amp; @suewatling. Part of the Look Again campaign with @PeterGlen17 https://t.co/vIcslKM2WR https://t.co/wtfoWlm5jw"
+	}
+]

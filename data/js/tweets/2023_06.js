daf100da73a27d2e8544ff5bfe9fc6785bca0c79
LeeFallin <lee@newgecko.net>
2023-06-09 01:21:17 +0100

    Added 1 tweets

---
+++ data/js/tweets/2023_06.js
@@ -0,0 +1,214 @@
+Grailbird.data.tweets_2023_06 = 
+[
+	{
+		"created_at": "Thu Jun 08 14:03:00 +0000 2023",
+		"id": 1666807397523005400,
+		"id_str": "1666808044565708951",
+		"full_text": "Introducing speaker Lee Fallin at #ALDcon23 ! Get ready to be inspired and gain insights from his expertise. Read Lee's profile on the conference website - https://t.co/4OcMMkF6HQ  #LoveLD @LeeFallin https://t.co/9bw441seRy",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			199
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "ALDcon23",
+					"indices": [
+						34,
+						43
+					]
+				},
+				{
+					"text": "LoveLD",
+					"indices": [
+						181,
+						188
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "LeeFallin",
+					"name": "Dr Lee Fallin",
+					"id": 55448837,
+					"id_str": "55448837",
+					"indices": [
+						189,
+						199
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/4OcMMkF6HQ",
+					"expanded_url": "https://shorturl.at/bitEV",
+					"display_url": "shorturl.at/bitEV",
+					"indices": [
+						156,
+						179
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1666807395279073300,
+					"id_str": "1666807395279073286",
+					"indices": [
+						200,
+						223
+					],
+					"media_url": "http://pbs.twimg.com/media/FyGwfrCacAYxKvU.png",
+					"media_url_https": "https://pbs.twimg.com/media/FyGwfrCacAYxKvU.png",
+					"url": "https://t.co/9bw441seRy",
+					"display_url": "pic.twitter.com/9bw441seRy",
+					"expanded_url": "https://twitter.com/aldinhe_LH/status/1666807397523005440/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 680,
+							"h": 671,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 777,
+							"h": 767,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 777,
+							"h": 767,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1666807395279073300,
+					"id_str": "1666807395279073286",
+					"indices": [
+						200,
+						223
+					],
+					"media_url": "http://pbs.twimg.com/media/FyGwfrCacAYxKvU.png",
+					"media_url_https": "https://pbs.twimg.com/media/FyGwfrCacAYxKvU.png",
+					"url": "https://t.co/9bw441seRy",
+					"display_url": "pic.twitter.com/9bw441seRy",
+					"expanded_url": "https://twitter.com/aldinhe_LH/status/1666807397523005440/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 680,
+							"h": 671,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 777,
+							"h": 767,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 777,
+							"h": 767,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://www.hootsuite.com\" rel=\"nofollow\">Hootsuite Inc.</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 119064069,
+			"id_str": "119064069",
+			"name": "ALDinHE and LearnHigher",
+			"screen_name": "aldinhe_LH",
+			"location": "",
+			"description": "Association for Learning Development in Higher Education: empowering Learning Developers to empower students in Higher Education. Find our journal at @JLDHE1",
+			"url": "https://t.co/lfFXnPsRG2",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/lfFXnPsRG2",
+							"expanded_url": "https://aldinhe.ac.uk",
+							"display_url": "aldinhe.ac.uk",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 2623,
+			"friends_count": 2992,
+			"listed_count": 37,
+			"created_at": "Tue Mar 02 15:21:07 +0000 2010",
+			"favourites_count": 2499,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 4974,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1013736217937137664/g4Ydq7j-_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1013736217937137664/g4Ydq7j-_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/119064069/1620115551",
+			"profile_link_color": "981CEB",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1,
+		"favorite_count": 4,
+		"favorited": true,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Introducing speaker Lee Fallin at #ALDcon23 ! Get ready to be inspired and gain insights from his expertise. Read Lee's profile on the conference website - https://t.co/4OcMMkF6HQ  #LoveLD @LeeFallin https://t.co/9bw441seRy"
+	}
+]
